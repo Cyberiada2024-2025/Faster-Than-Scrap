94a39842e47b75e62c2912199536a2fb4edd5786
Vertical-Edek <157529422+Vertical-Edek@users.noreply.github.com>
2025-01-12 17:55:23 +0100

    Space environment shader prefab
    Space environment shader added as prefab

---
+++ faster-than-scrap/art/materials/environment/space_environment.gd
@@ -0,0 +1,28 @@
+extends WorldEnvironment
+
+@export var rotation_speed : float = 4
+@export var bloom_speed : float = 8
+@export var bloom_amplitude : float = 0.2
+@export var bloom_offset : float = 0.5
+
+var time : float = 0;
+
+@onready var SpaceEnv : Environment = get_environment()
+
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+	
+	# Texture rotation
+	SpaceEnv.sky_rotation.y += fmod( delta * rotation_speed * 0.001 , 2 * PI )
+	
+	# Time calculaton with respect to bloom speed
+	time += delta * bloom_speed * 0.1
+	time = fmod( time, (2 * PI) )
+	
+	# Bloom animation
+	var bloom_value = bloom_offset + bloom_amplitude * sin(time)
+	SpaceEnv.set_glow_bloom(bloom_value)

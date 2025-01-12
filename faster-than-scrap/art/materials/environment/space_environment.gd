6436dbed593e3e9b87302bc14ad997dce6d9c4c6
Vertical-Edek <157529422+Vertical-Edek@users.noreply.github.com>
2025-01-12 18:58:35 +0100

    Linter 3

--- faster-than-scrap/art/materials/environment/space_environment.gd
+++ faster-than-scrap/art/materials/environment/space_environment.gd
@@ -12,11 +12,9 @@ var time : float = 0;
 func _process(delta: float) -> void:
 	# Texture rotation
 	space_env.sky_rotation.y += fmod( delta * rotation_speed * 0.001 , 2 * PI )
-	
 	# Time calculaton with respect to bloom speed
 	time += delta * bloom_speed * 0.1
 	time = fmod( time, (2 * PI) )
-	
 	# Bloom animation
 	var bloom_value = bloom_offset + bloom_amplitude * sin(time)
 	space_env.set_glow_bloom(bloom_value)

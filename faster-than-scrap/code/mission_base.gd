fbd2ca7ac59f7dfb127f1a3ccc0236c94c7d9988
Mikul-xyz <fmikulski@o2.pl>
2025-01-13 19:38:33 +0100

    ,

--- faster-than-scrap/code/mission_base.gd
+++ faster-than-scrap/code/mission_base.gd
@@ -7,10 +7,6 @@ var about : String = ""
 var type : MissionType
 var difficulty : int
 
-#var reward : 
-
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.

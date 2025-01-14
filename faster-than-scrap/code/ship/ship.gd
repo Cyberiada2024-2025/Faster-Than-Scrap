208b67027c74c176d725f7cf9969d893a839a455
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-01-14 23:07:42 +0100

    use energy ship interface

--- faster-than-scrap/code/ship/ship.gd
+++ faster-than-scrap/code/ship/ship.gd
@@ -2,6 +2,23 @@ class_name Ship
 
 extends Node3D
 
+var energy: float = 100
+   
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+## Called when module wants to use the ship's energy [member Ship.energy].
+## Returns true when it can afford that amount (and reduces the energy accordingly)
+## otherwise returns false and doesn't change the energy amount.
+func use_energy(amount: float) -> bool:
+	if energy<amount:
+		return false
+	else:
+		energy -= amount
+		_on_energy_change()
+		return true
+
+## Called whenever the energy amount changes.
+func _on_energy_change() -> void:
+	pass

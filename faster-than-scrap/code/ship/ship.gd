bb3375beaa8092faaf0d20b0f063af5943bacaad
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-01-15 04:01:24 +0100

    Weapon module prototype

--- faster-than-scrap/code/ship/ship.gd
+++ faster-than-scrap/code/ship/ship.gd
@@ -2,6 +2,9 @@ class_name Ship
 
 extends Node3D
 
+
+var energy: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.

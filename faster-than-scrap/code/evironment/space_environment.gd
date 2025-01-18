59d8272e3006b4372d9f22d1adc77f0bcd367ec5
Vertical-Edek <157529422+Vertical-Edek@users.noreply.github.com>
2025-01-18 15:34:08 +0100

    Linter

--- faster-than-scrap/code/evironment/space_environment.gd
+++ faster-than-scrap/code/evironment/space_environment.gd
@@ -3,11 +3,11 @@ extends WorldEnvironment
 
 ## Texture rotation speed
 @export var rotation_speed : float = 4
-## Speed of [member Environment.glow_bloom] chagnes 
+## Speed of [member Environment.glow_bloom] chagnes
 @export var bloom_speed : float = 8
 ## Amplitude of [member Environment.glow_bloom] changes
 @export var bloom_amplitude : float = 0.2
-## Static [member Environment.glow_bloom] offset 
+## Static [member Environment.glow_bloom] offset
 @export var bloom_offset : float = 0.5
 
 var time : float = 0;
@@ -18,7 +18,7 @@ func _process(delta: float) -> void:
 	# Texture rotation
 	var rotation_amount : float = fmod( delta * rotation_speed * 0.001 , 2 * PI )
 	space_env.sky_rotation.y += rotation_amount
-	$NebulaLights.transform = $NebulaLights.transform.rotated(Vector3.UP, rotation_amount) 
+	$NebulaLights.transform = $NebulaLights.transform.rotated(Vector3.UP, rotation_amount)
 	# Time calculaton with respect to bloom speed
 	time += delta * bloom_speed * 0.1
 	time = fmod( time, (2 * PI) )

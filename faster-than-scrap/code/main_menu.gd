e187ac351686d43d89e8606c9049f476ec4bfd16
nata18629 <81750410+nata18629@users.noreply.github.com>
2025-01-21 20:54:25 +0100

    Main menu screen (#180)
    Issue #102 
    
    Main menu which contains:
    - game title (a placeholder graphic)
    - start button
    - exit button
    
    Focus on a button can be changed via arrows. 
    
    For the start button to work there needs to be specified a path to the
    main scene.

---
+++ faster-than-scrap/code/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Buttons/StartButton.grab_focus()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_start_button_pressed() -> void:
+	# TODO change to main scene
+	# get_tree().change_scene_to_file("res://scenes/main_scene")
+	pass

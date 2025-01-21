b2940417703d85a86f2f00b879059a2f875d8248
nata18629 <81750410+nata18629@users.noreply.github.com>
2025-01-21 18:29:34 +0100

    main menu fix

--- faster-than-scrap/code/main_menu.gd
+++ faster-than-scrap/code/main_menu.gd
@@ -4,10 +4,6 @@ extends Control
 func _ready() -> void:
 	$Buttons/StartButton.grab_focus()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 

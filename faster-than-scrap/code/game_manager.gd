7ed4bf395b8d0a61a12888cf90532c360fce7fb4
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-01-21 18:27:51 +0100

    Ship and cockpit script (#176)
    added ship base class, player ship and cockpit module.

--- faster-than-scrap/code/game_manager.gd
+++ faster-than-scrap/code/game_manager.gd
@@ -1,6 +1,44 @@
+class_name GameManager
+
 extends Node
+
+signal new_game_state
+
+@export var game_state :GameState.State = GameState.State.FLY
+@export var ship: PlayerShip
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+	if ship == null:
+		printerr("Game manager has player ship as nil")
+	else:
+		# connect signal to ship
+		# connecting should be moved elsewhere, when the main menu
+		# is implemented
+		new_game_state.connect(ship.on_game_change_state)
+
+
+func _set_game_state(new_state: GameState.State) -> void:
+	game_state=new_state
+	ship.on_game_change_state(new_state)
+	new_game_state.emit(new_state)
+	# future logic for changing state
+	match new_state:
+		GameState.State.FLY:
+			_unpause_entities()
+		GameState.State.PAUSE:
+			_pause_entities()
+		GameState.State.CUTSCENE:
+			_unpause_entities()
+		GameState.State.BUILD:
+			_pause_entities()
+		GameState.State.MAIN_MENU:
+			_unpause_entities()
+
+func _pause_entities():
+	get_tree().root.paused = true
+
+func _unpause_entities():
+	get_tree().root.paused = false
--- faster-than-scrap/code/ship/modules/module.gd
+++ faster-than-scrap/code/ship/modules/module.gd
@@ -9,24 +9,20 @@ extends Node3D
 @export var hp: int = 100
 @export var ship: Ship
 
-## whether module should react to keyboard input (should be inactive when the game is paused)
-@export var active: bool = true
-
 var was_key_pressed: bool = false
 
 
 func _process(_delta: float) -> void:
-	if active:
-		if was_key_pressed:
-			if Input.is_key_pressed(activation_key):
-				_on_key(_delta)
-			else:
-				was_key_pressed = false
-				_on_release(_delta)
+	if was_key_pressed:
+		if Input.is_key_pressed(activation_key):
+			_on_key(_delta)
 		else:
-			if Input.is_key_pressed(activation_key):
-				_on_key_press(_delta)
-				was_key_pressed = true
+			was_key_pressed = false
+			_on_release(_delta)
+	else:
+		if Input.is_key_pressed(activation_key):
+			_on_key_press(_delta)
+			was_key_pressed = true
 
 # virtual functions
 func _on_key_press(_delta: float) -> void:
@@ -58,3 +54,6 @@ func _explode() -> void:
 	# TODO create particles object,
 	# which will die after some die by itself
 	pass
+
+func detachable() -> bool:
+	return true

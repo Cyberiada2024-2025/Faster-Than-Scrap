fff99bd57b72c47f3b01307b9df77c58cd05f020
Executed419 <lucker10@interia.pl>
2025-01-26 21:04:26 +0100

    base nodes for state machine implementation

---
+++ faster-than-scrap/code/enemy/states/idle.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func enter(previous_state_path: String, data := {}) -> void:
+	#set parameters
+	pass
+
+func physics_update(_delta: float) -> void:
+	# move in random direction, changeing every few seconds
+	pass
---
+++ faster-than-scrap/code/enemy/states/state_ship_enemy.gd
@@ -0,0 +1,15 @@
+class_name StateEnemyShip extends State
+
+
+const IDLE = "Idle"
+#const CHASE = "Chase" # may be too similar to aggresive
+const AGGRESIVE = "Aggresive" 
+const DEFENSIVE = "Defensive"
+
+var enemy: Ship
+
+
+func _ready() -> void:
+	await owner.ready
+	enemy = owner as Ship	# getting a typed reference to controlled ship
+	assert(enemy != null, "The EnemyState state type must be used only in the enemy scene. It needs the owner to be an Enemy node.")

ff1f8dbc2a651d2f4c47fdda022ad7eed0e0ddbc
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-01-28 14:35:24 +0100

    scene manager + new scenes

--- faster-than-scrap/code/Singletons/game_manager.gd
+++ faster-than-scrap/code/Singletons/game_manager.gd
@@ -1,28 +1,22 @@
-class_name GameManager
-
 extends Node
+
+## Game manager is a singleton!
+## It is not supposed to be added to the scene tree!
+## It is autoloaded and always available to be called from everywhere!
 
 signal new_game_state
 
 @export var game_state :GameState.State = GameState.State.FLY
-@export var ship: PlayerShip
+@export var player_ship: PlayerShip
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_PAUSABLE
-	if ship == null:
-		printerr("Game manager has player ship as nil")
-	else:
-		# connect signal to ship
-		# connecting should be moved elsewhere, when the main menu
-		# is implemented
-		new_game_state.connect(ship.on_game_change_state)
 
 
-func _set_game_state(new_state: GameState.State) -> void:
+func set_game_state(new_state: GameState.State) -> void:
 	game_state=new_state
-	ship.on_game_change_state(new_state)
 	new_game_state.emit(new_state)
 	# future logic for changing state
 	match new_state:
@@ -38,7 +32,7 @@ func _set_game_state(new_state: GameState.State) -> void:
 			_unpause_entities()
 
 func _pause_entities():
-	get_tree().root.paused = true
+	get_tree().paused = true
 
 func _unpause_entities():
-	get_tree().root.paused = false
+	get_tree().paused = false

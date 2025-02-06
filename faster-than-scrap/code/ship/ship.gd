d7205b68fa52888b7b1e70a534a1b72b37a907b9
Paweł Szczepański <107117250+PawelSzczepan@users.noreply.github.com>
2025-02-06 19:40:38 +0100

    Shield (#183)
    To use shield use the "Shield" prefab
    
    For test script damaging on Space, and turing on/off on W
    
    Integrated sounds
    
    Added some animations for shield
    
    
    https://github.com/user-attachments/assets/0dcd77cd-570d-49de-bde6-d4b696862aa9

--- faster-than-scrap/code/ship/ship.gd
+++ faster-than-scrap/code/ship/ship.gd
@@ -6,9 +6,10 @@ extends Node3D
 
 @export var energy: float = 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	energy += restore * delta
 
 ## Called when module wants to use the ship's energy [member Ship.energy].
 ## Returns true when it can afford that amount (and reduces the energy accordingly)

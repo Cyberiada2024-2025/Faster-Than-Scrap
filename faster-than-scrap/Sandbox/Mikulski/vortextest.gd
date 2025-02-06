38f9a5df66145ce441f1fece82785d36ce806ef8
Mikul-xyz <fmikulski@o2.pl>
2025-02-06 19:41:31 +0100

    Space vortex script (#187)
    Space vortex script #161
    
    Co-authored-by: Paweł Szczepański <107117250+PawelSzczepan@users.noreply.github.com>

---
+++ faster-than-scrap/Sandbox/Mikulski/vortextest.gd
@@ -0,0 +1,18 @@
+extends CharacterBody3D
+#extends RigidBody3D
+#extends Area3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print_debug("test\n")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x += delta *3
+
+
+
+func take_damage(damage: int) -> void:
+	print_debug("dmaged")
+	print_debug(damage)

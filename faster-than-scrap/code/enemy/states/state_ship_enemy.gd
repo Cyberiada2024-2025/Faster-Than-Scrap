215890074a814fdbd2edac589eb9bd9c0e9c7bea
Executed419 <lucker10@interia.pl>
2025-02-09 10:49:33 +0100

    linter3

--- faster-than-scrap/code/enemy/states/state_ship_enemy.gd
+++ faster-than-scrap/code/enemy/states/state_ship_enemy.gd
@@ -11,4 +11,5 @@ var enemy: Ship
 func _ready() -> void:
 	await owner.ready
 	enemy = owner as Ship	# getting a typed reference to controlled ship
-	assert(enemy != null, "The EnemyState state type must be used only in the enemy scene. It needs the owner to be an Enemy node.")
+	assert(enemy != null, "The EnemyState state type must be used only in the enemy scene. 
+It needs the owner to be an Enemy node.")

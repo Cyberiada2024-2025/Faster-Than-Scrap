bb9ee371ee85f79315a0dbb990f0268bcd5a176c
Mikul-xyz <fmikulski@o2.pl>
2025-02-11 17:39:34 +0100

    asteroid script

---
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -0,0 +1,35 @@
+extends RigidBody3D
+
+@export_category('Stats')
+@export var hp : int = 100
+@export var damage_multiplier : int = 1
+@export_group('StartValues')
+@export var START_SPEED_RANGE : float = 5.0
+@export var START_SIZE_RANGE : float = 5.0
+
+
+func _ready() -> void:
+	self.linear_velocity.x = randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
+	self.linear_velocity.z = randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
+	self.scale *= randf_range(1.0, START_SIZE_RANGE)
+	hp *= self.scale.x
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func take_damage(damage: int) -> void:
+	hp -= damage
+	print('damaged')
+	if hp <=0 :
+		_on_destroy()
+		print('detroyed')
+
+func _on_destroy() -> void:
+	queue_free()
+
+
+func _on_body_entered(body: Node) -> void:
+	print('hit')
+	body.take_damage(self.scale.x * self.linear_velocity.length() * damage_multiplier)

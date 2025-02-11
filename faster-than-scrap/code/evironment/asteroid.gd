62c96c03e297ec777aeb2bff8ecf27323f5dbee3
Mikul-xyz <fmikulski@o2.pl>
2025-02-11 18:05:23 +0100

    damaged group

--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -2,10 +2,10 @@ extends RigidBody3D
 
 @export_category('Stats')
 @export var hp : int = 100
-@export var damage_multiplier : int = 1
+@export var damage_multiplier : int = 10
 @export_group('StartValues')
-@export var START_SPEED_RANGE : float = 5.0
-@export var START_SIZE_RANGE : float = 5.0
+@export var START_SPEED_RANGE : float = 10.0
+@export var START_SIZE_RANGE : float = 10.0
 
 
 func _ready() -> void:
@@ -22,14 +22,16 @@ func _process(delta: float) -> void:
 func take_damage(damage: int) -> void:
 	hp -= damage
 	print('damaged')
+	print(damage)
 	if hp <=0 :
-		_on_destroy()
 		print('detroyed')
+		_on_destroy()
 
 func _on_destroy() -> void:
 	queue_free()
 
 
 func _on_body_entered(body: Node) -> void:
-	print('hit')
-	body.take_damage(self.scale.x * self.linear_velocity.length() * damage_multiplier)
+	if body.is_in_group("take damege from asteroids"):
+		print('hit')
+		body.take_damage(((self.scale.x * self.linear_velocity.length()) + body.linear_velocity.length()) * damage_multiplier)

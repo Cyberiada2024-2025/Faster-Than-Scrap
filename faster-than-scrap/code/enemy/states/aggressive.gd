ae892b1e8f384e6797375946b4ba54b476fffec0
Executed419 <lucker10@interia.pl>
2025-02-17 08:53:13 +0100

    Moved movement to StateEnemy class

--- faster-than-scrap/code/enemy/states/aggressive.gd
+++ faster-than-scrap/code/enemy/states/aggressive.gd
@@ -6,20 +6,18 @@ class_name EnemyAggressive
 
 # should inherit speed from Ship node
 @export var speed: float
-@export var max_distance: float
+@export var min_range_to_player: float
 func enter(_previous_state_path: String, _data := {}) -> void:
 	#set parameters
 	target = get_tree().get_first_node_in_group("Player")
 
 func state_physics_update(delta: float) -> void:
-	var vector_to_target = target.global_position - enemy.global_position
-	var direction = vector_to_target.normalized()
-	
-	if vector_to_target.length() > max_distance:
-		#enemy.position += direction * speed * delta
-		enemy.velocity = direction*speed*delta;
-		enemy.move_and_slide()
-	enemy.look_at(target.position)
-	#else:
-		#enemy.velocity = Vector3()
+	move_target_spotted(min_range_to_player, target)
+	#var vector_to_target = target.global_position - enemy.global_position
+	#var direction = vector_to_target.normalized()
+	#
+	#if vector_to_target.length() > max_distance:
+		#enemy.velocity = direction*speed*delta;
+		#enemy.move_and_slide()
+	#enemy.look_at(target.position)
 		
--- faster-than-scrap/code/enemy/states/state_enemy.gd
+++ faster-than-scrap/code/enemy/states/state_enemy.gd
@@ -12,3 +12,12 @@ func _ready() -> void:
 	await owner.ready
 	enemy = owner as Ship	# getting a typed reference to controlled ship
 	assert(enemy != null, "The enemy state needs the owner to be an Enemy node")
+
+func move_target_spotted(min_range_to_player: int, target: Ship) -> void:
+	var vector_to_target = target.global_position - enemy.global_position
+	var direction = vector_to_target.normalized()
+	
+	if vector_to_target.length() > min_range_to_player:
+		enemy.velocity = direction*enemy.speed;
+		enemy.move_and_slide()
+	enemy.look_at(target.position)

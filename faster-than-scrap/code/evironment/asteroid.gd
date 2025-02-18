2cbfd2366c955ad9e65b58bf890e05631103fea8
Mikul-xyz <fmikulski@o2.pl>
2025-02-18 01:32:04 +0100

    self damage

--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -3,16 +3,15 @@ extends RigidBody3D
 @export_category('Stats')
 @export var hp : int = 100
 @export var damage_multiplier : int = 10
+@export var self_damage_multiplier : float = 0.3
 @export_group('StartValues')
 @export var START_SPEED_RANGE : float = 10.0
 @export var START_SIZE_RANGE : float = 10.0
 
 
 func _ready() -> void:
-	self.linear_velocity.x = randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
-	self.linear_velocity.z = randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
-	self.scale *= randf_range(1.0, START_SIZE_RANGE)
-	hp *= self.scale.x
+	self.linear_velocity.x += randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
+	self.linear_velocity.z += randf_range(-START_SPEED_RANGE, START_SPEED_RANGE)
 
 
 func _process(delta: float) -> void:
@@ -21,10 +20,7 @@ func _process(delta: float) -> void:
 
 func take_damage(damage: int) -> void:
 	hp -= damage
-	print('damaged')
-	print(damage)
 	if hp <=0 :
-		print('detroyed')
 		_on_destroy()
 
 func _on_destroy() -> void:
@@ -32,6 +28,8 @@ func _on_destroy() -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("take damege from asteroids"):
-		print('hit')
-		body.take_damage(((self.scale.x * self.linear_velocity.length()) + body.linear_velocity.length()) * damage_multiplier)
+	if body.is_in_group("take damege from colisions"):
+		var direction : Vector3 = self.position.direction_to(body.position).abs()
+		var damage : int = self.scale.x * damage_multiplier * (self.linear_velocity + body.linear_velocity).abs().dot(direction)
+		body.take_damage(damage)
+		self.take_damage(self_damage_multiplier*damage)

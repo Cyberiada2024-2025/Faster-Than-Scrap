a95d23d802afd6f901cad625fbff731ec750d036
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-02-25 20:27:01 +0100

    Do not delete ship on change scene (#217)
    solves #205, fixes #193
    Custom test scene for checking if the ship is kept and its hud

--- faster-than-scrap/code/Player/hud.gd
+++ faster-than-scrap/code/Player/hud.gd
@@ -1,11 +1,8 @@
-@tool
 class_name Hud
 
 extends Node3D
 
 @export var energy_bar: ResourceBar
-
-@export var player_ship: Node3D = null
 
 @export var main_camera_offset: Vector3 = Vector3(0,10,0)
 @export var module_camera_offset: Vector3 = Vector3(0,10,0)
@@ -25,11 +22,7 @@ func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 	# keep camera offsets relatively to player
+	var player_ship = GameManager.player_ship
 	_main_camera.global_position = player_ship.global_position + main_camera_offset
 	_module_camera.global_position = player_ship.global_position + module_camera_offset
 	_minimap_camera.global_position = player_ship.global_position + minimap_camera_offset
-
-func _get_configuration_warnings() -> PackedStringArray:
-	if player_ship == null:
-		return ["Field \"playerShip\" not set"]
-	return []

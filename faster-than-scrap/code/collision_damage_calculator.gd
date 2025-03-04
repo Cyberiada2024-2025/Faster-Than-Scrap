1c86dc5a07e5c20d2ea2e4a81043530c6ee0e438
Mikul-xyz <fmikulski@o2.pl>
2025-03-04 20:42:24 +0100

    outside collison calculator

--- faster-than-scrap/code/collision_damage_calculator.gd
+++ faster-than-scrap/code/collision_damage_calculator.gd
@@ -1,34 +1,37 @@
 extends Node3D
 
 
-# return Dictionary:
-# 	"self_damage" - damage dealt to yourself
-# 	"dealt_damage" - damage dealt to oponent
+func _ready() -> void:
+	get_parent().body_entered.connect(_find_parent_collision)
+	get_parent().contact_monitor = true
+	if get_parent().max_contacts_reported <=0:
+		get_parent().max_contacts_reported = 10
+
+
+func _find_parent_collision(body: Node) -> void:
+	if body.is_in_group("deal damege by collisions"):
+		var damage: int = calculate_damage(get_parent(), body)
+		get_parent().take_damage(damage)
+
+
 # from atribute nodes include:
 # 	dealt_damage_multiplier
 # 	self_damage_multiplier
-func calculate_damage(me: Node, oponent: Node) -> Dictionary:
+func calculate_damage(me: Node, oponent: Node) -> int:
 	# calculate base damage
 	var direction: Vector3 = me.position.direction_to(oponent.position)
-	var base_damage: int = (me.linear_velocity + oponent.linear_velocity).dot(direction)
+	var base_damage: float = (me.linear_velocity + oponent.linear_velocity).dot(direction)
 	base_damage = abs(base_damage)
-	base_damage = me.mass * oponent.mass * base_damage
-
 	# get multiplies
-	var dealt_dmg_mult: float = 1
-	if "dealt_damage_multiplier" in me:
-		dealt_dmg_mult *= me.dealt_damage_multiplier
-	if "self_damage_multiplier" in oponent:
-		dealt_dmg_mult *= oponent.self_damage_multiplier
-
 	var self_dmg_mult: float = 1
 	if "self_damage_multiplier" in me:
 		self_dmg_mult *= me.self_damage_multiplier
 	if "dealt_damage_multiplier" in oponent:
 		self_dmg_mult *= oponent.dealt_damage_multiplier
 
-	# pack results
-	var result: Dictionary
-	result["dealt_damage"] = base_damage * dealt_dmg_mult
-	result["self_damage"] = base_damage * self_dmg_mult
-	return result
+	# return result
+	var damage: float = base_damage * self_dmg_mult
+	if "mass" in oponent:
+		damage *= oponent.mass / (me.mass + oponent.mass)
+
+	return (int)(damage)
--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -1,11 +1,9 @@
 extends RigidBody3D
 
-@export_category("Nodes")
-@export var collision_damage_calculator: Node
 @export_category("Stats")
 @export var hp: int = 100
-@export var dealt_damage_multiplier: int = 10
-@export var self_damage_multiplier: int = 3
+@export var dealt_damage_multiplier: int = 1
+@export var self_damage_multiplier: int = 1
 @export_group("StartValues")
 @export var start_speed_range: float = 10.0
 
@@ -23,10 +21,3 @@ func take_damage(damage: int) -> void:
 
 func _on_destroy() -> void:
 	queue_free()
-
-
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("take damege from collisions"):
-		var damage: Dictionary = collision_damage_calculator.calculate_damage(self, body)
-		body.take_damage(damage["dealt_damage"])
-		self.take_damage(damage["self_damage"])

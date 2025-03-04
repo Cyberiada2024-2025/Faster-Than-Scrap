3e719c5a27b55c6d451183ea5079883deea248bc
Mikul-xyz <fmikulski@o2.pl>
2025-03-04 14:38:33 +0100

    autofrmater

--- faster-than-scrap/code/collision_damage_calculator.gd
+++ faster-than-scrap/code/collision_damage_calculator.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+
 # return Dictionary:
 # 	"self_damage" - damage dealt to yourself
 # 	"dealt_damage" - damage dealt to oponent
@@ -8,26 +9,26 @@ extends Node3D
 # 	self_damage_multiplier
 func calculate_damage(me: Node, oponent: Node) -> Dictionary:
 	# calculate base damage
-	var direction : Vector3 = me.position.direction_to(oponent.position)
-	var base_damage : int = (me.linear_velocity + oponent.linear_velocity).dot(direction)
+	var direction: Vector3 = me.position.direction_to(oponent.position)
+	var base_damage: int = (me.linear_velocity + oponent.linear_velocity).dot(direction)
 	base_damage = abs(base_damage)
 	base_damage = me.mass * oponent.mass * base_damage
-	
+
 	# get multiplies
-	var dealt_dmg_mult : float = 1
-	if ("dealt_damage_multiplier" in me):
+	var dealt_dmg_mult: float = 1
+	if "dealt_damage_multiplier" in me:
 		dealt_dmg_mult *= me.dealt_damage_multiplier
-	if ("self_damage_multiplier" in oponent):
+	if "self_damage_multiplier" in oponent:
 		dealt_dmg_mult *= oponent.self_damage_multiplier
-		
-	var self_dmg_mult : float = 1
-	if ("self_damage_multiplier" in me):
+
+	var self_dmg_mult: float = 1
+	if "self_damage_multiplier" in me:
 		self_dmg_mult *= me.self_damage_multiplier
-	if ("dealt_damage_multiplier" in oponent):
+	if "dealt_damage_multiplier" in oponent:
 		self_dmg_mult *= oponent.dealt_damage_multiplier
-	
+
 	# pack results
-	var result : Dictionary
+	var result: Dictionary
 	result["dealt_damage"] = base_damage * dealt_dmg_mult
 	result["self_damage"] = base_damage * self_dmg_mult
 	return result
--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -1,13 +1,13 @@
 extends RigidBody3D
 
-@export_category('Nodes')
-@export var collision_damage_calculator : Node
-@export_category('Stats')
-@export var hp : int = 100
-@export var dealt_damage_multiplier : int = 10
-@export var self_damage_multiplier : int = 3
-@export_group('StartValues')
-@export var start_speed_range : float = 10.0
+@export_category("Nodes")
+@export var collision_damage_calculator: Node
+@export_category("Stats")
+@export var hp: int = 100
+@export var dealt_damage_multiplier: int = 10
+@export var self_damage_multiplier: int = 3
+@export_group("StartValues")
+@export var start_speed_range: float = 10.0
 
 
 func _ready() -> void:
@@ -15,11 +15,11 @@ func _ready() -> void:
 	self.linear_velocity.z += randf_range(-start_speed_range, start_speed_range)
 
 
-
 func take_damage(damage: int) -> void:
 	hp -= damage
-	if hp <=0 :
+	if hp <= 0:
 		_on_destroy()
+
 
 func _on_destroy() -> void:
 	queue_free()
@@ -27,6 +27,6 @@ func _on_destroy() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("take damege from collisions"):
-		var damage:Dictionary = collision_damage_calculator.calculate_damage(self, body)
+		var damage: Dictionary = collision_damage_calculator.calculate_damage(self, body)
 		body.take_damage(damage["dealt_damage"])
 		self.take_damage(damage["self_damage"])

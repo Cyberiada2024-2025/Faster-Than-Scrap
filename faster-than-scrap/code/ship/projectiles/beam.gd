9180a59bd7dd428fd602512b80b67feab25579da
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-03-05 15:00:28 +0100

    Removed die on hit from beam

--- faster-than-scrap/code/ship/projectiles/beam.gd
+++ faster-than-scrap/code/ship/projectiles/beam.gd
@@ -10,17 +10,11 @@ extends Node3D
 ## Node that will be scaled so that it's Z size is the same as the beam's length.
 @export var beam_indicator: Node3D
 
-## If true, the beam will die immediately after hitting something.
-## Otherwise it will keep going until it is destroyed by other means. [br]
-## Note: In both cases, die_on_hit in the child [DamageRaycast3D] should be set to false.
-@export var die_on_hit: bool = false
-
 var _beam_length: float
 
 
 func _ready() -> void:
 	_beam_length = _damage_raycast.target_position.length()
-	_damage_raycast.damage_applied.connect(_on_damage_applied)
 
 
 func _process(delta: float) -> void:
@@ -29,8 +23,3 @@ func _process(delta: float) -> void:
 		beam_indicator.scale.z = ray_length
 	else:
 		beam_indicator.scale.z = _beam_length
-
-
-func _on_damage_applied(_damage: Damage, _target: Damageable):
-	if die_on_hit:
-		queue_free()

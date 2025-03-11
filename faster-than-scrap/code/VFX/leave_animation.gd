4af079650f315505b76b91295a5a0b5e4a35af6c
Mikul-xyz <fmikulski@o2.pl>
2025-03-11 19:05:34 +0100

    prepare to jump

--- faster-than-scrap/code/VFX/leave_animation.gd
+++ faster-than-scrap/code/VFX/leave_animation.gd
@@ -1,63 +1,44 @@
 extends Node
 
-signal after_finished
-
 @export_category("Animation")
 @export var leaving_body: Node3D
-@export_group("Curve")
-@export var rotation_curve: Curve
-@export var velocity_curve: Curve
-@export var scale_curve: Curve
-@export var position_curve: Curve
+@export var particles: CPUParticles3D
 @export_group("Animation Fragments Times")
-@export var rotation_time: float = 3.0
-@export var velocity_time: float = 3.0
-@export var jump_time: float = 1.0
+@export var jump_preparation_time: float = 5.0
+@export var jump_time: float = 2.0
 
-var start_angle: float
-var start_linear_velocity: Vector3
-var start_angular_velocity: Vector3
-var start_scale: float
-var start_position: float
+var ending: Callable
+var position_base: Vector3
+var last_time: float = 0
 
 
 func _ready() -> void:
-	start_animation()  # to del
 	pass
 
 
-func start_animation() -> void:
-	start_angle = leaving_body.rotation.y
-	start_linear_velocity = leaving_body.linear_velocity
-	start_angular_velocity = leaving_body.angular_velocity
-	start_scale = leaving_body.scale.z
-
-	var tween = get_tree().create_tween().set_parallel(true)
-	tween.tween_method(rotation_tween_metod, 0.0, 1.0, rotation_time)
-	tween.tween_method(velocity_tween_metod, 0.0, 1.0, velocity_time)
-	tween.chain().tween_callback(prepare_to_jump)
-	tween.chain().tween_method(jump, 0.0, 1.0, jump_time)
-	tween.chain().tween_callback(end_animation)
-
-
-func rotation_tween_metod(time: float) -> void:
-	leaving_body.rotation.y = start_angle * rotation_curve.sample(time)
-
-
-func velocity_tween_metod(time: float) -> void:
-	leaving_body.linear_velocity = start_linear_velocity * velocity_curve.sample(time)
-	leaving_body.angular_velocity = start_angular_velocity * velocity_curve.sample(time)
+func start_animation(ending_method: Callable) -> void:
+	self.ending = ending_method
+	var tween = get_tree().create_tween()
+	tween.tween_callback(prepare_to_jump)
+	tween.tween_interval(jump_preparation_time)
+	tween.tween_callback(jump)
+	tween.tween_interval(jump_time)
+	tween.tween_callback(end_animation).set_delay(jump_time)
 
 
 func prepare_to_jump() -> void:
-	start_position = leaving_body.position.z
+	particles.reparent(leaving_body.cockpit)
+	particles.emitting = true
+	pass
 
 
-func jump(time: float) -> void:
-	leaving_body.scale.z = start_scale * scale_curve.sample(time)
-	leaving_body.position.z = start_position + start_scale * position_curve.sample(time)
+func jump() -> void:
+	particles.emitting = false
+	particles.reparent(self)
+	leaving_body.visible = false
+	pass
 
 
 func end_animation() -> void:
-	print("end")
-	pass
+	leaving_body.visible = true
+	self.ending.call()

9658d3dbb18188483b86e70a9f23c6c0a14e1a61
Mikul-xyz <fmikulski@o2.pl>
2025-03-11 19:55:02 +0100

    jump

--- faster-than-scrap/code/VFX/leave_animation.gd
+++ faster-than-scrap/code/VFX/leave_animation.gd
@@ -3,9 +3,10 @@ extends Node
 @export_category("Animation")
 @export var leaving_body: Node3D
 @export var prepare_particles: CPUParticles3D
+@export var jump_player: AnimationPlayer
 @export_group("Animation Fragments Times")
 @export var jump_preparation_time: float = 5.0
-@export var jump_time: float = 2.0
+@export var jump_time: float = 1.0
 
 var ending: Callable
 var position_base: Vector3
@@ -13,7 +14,7 @@ var last_time: float = 0
 
 
 func _ready() -> void:
-	pass
+	jump_player.speed_scale = 1 / jump_time
 
 
 func start_animation(ending_method: Callable) -> void:
@@ -35,6 +36,8 @@ func jump() -> void:
 	prepare_particles.emitting = false
 	prepare_particles.reparent(self)
 	leaving_body.visible = false
+	jump_player.get_child(0).position = leaving_body.cockpit.position
+	jump_player.play("jump")
 
 
 func end_animation() -> void:

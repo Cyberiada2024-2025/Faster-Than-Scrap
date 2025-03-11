c6c242085ec7d90d990456303a79e03217c305d5
Mikul-xyz <fmikulski@o2.pl>
2025-03-11 19:10:12 +0100

    linter

--- faster-than-scrap/sandbox/mikulski/set_player_for_test.gd
+++ faster-than-scrap/sandbox/mikulski/set_player_for_test.gd
@@ -5,10 +5,8 @@ extends Node
 var time: float = 0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.player_ship = player
-	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,5 +18,3 @@ func _process(delta: float) -> void:
 		#time -= 0.5
 	else:
 		time += delta
-
-	pass

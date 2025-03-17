9adec39f1ebd1d25cb455b912c31ab6bfcabad00
Mikul-xyz <fmikulski@o2.pl>
2025-03-17 15:01:23 +0100

    exit credits on key press
    added ending credits after anything is pressed

--- faster-than-scrap/code/credits.gd
+++ faster-than-scrap/code/credits.gd
@@ -8,6 +8,15 @@ func _ready() -> void:
 	animationPlayer.play("credits_text")
 
 
+func exit_credits() -> void:
+	sceneLoader.load_main_menu_scene()
+
+
+func _process(_delta: float) -> void:
+	if Input.is_anything_pressed():
+		exit_credits()
+
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "credits_text":
-		sceneLoader.load_main_menu_scene()
+		exit_credits()

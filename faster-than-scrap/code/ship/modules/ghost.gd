eea607ada82b533cf74ada0a5aeab447f6514194
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-18 20:25:02 +0100

    Add ignore ghost collision with original module

--- faster-than-scrap/code/ship/modules/ghost.gd
+++ faster-than-scrap/code/ship/modules/ghost.gd
@@ -3,7 +3,7 @@ class_name ModuleGhost
 extends Area3D
 
 var collided_modules: Array[Module]
-
+var module_to_ignore: Module
 
 func _ready():
 	body_shape_entered.connect(_on_body_shape_entered)
@@ -13,8 +13,9 @@ func _ready():
 func _on_body_shape_entered(
 	_body_rid: RID, body: Node3D, body_shape_index: int, _local_shape_index: int
 ):
-	collided_modules.append(body.get_child(body_shape_index))
-
+	var collided = body.get_child(body_shape_index)
+	if collided != module_to_ignore :
+		collided_modules.append(collided)
 
 func _on_body_shape_exited(
 	_body_rid: RID, body: Node3D, body_shape_index: int, _local_shape_index: int

1d69cab2ea9a4676f2281d8116fb04e313508a38
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-22 19:22:10 +0100

    Fix collision damage to new modules (#276)
    Added collision damage all modules.
    Fixed so it would work with new modules and to make it compatible with
    other type of objects (damageble).
    
    Can be tested in sandboxes:
    - mikulski/asteroids_test
    - Wierzba/flyable_ship/fly_fly (hit the cube to damage the thruster)

--- faster-than-scrap/code/collision_damage_calculator.gd
+++ faster-than-scrap/code/collision_damage_calculator.gd
@@ -1,19 +1,29 @@
 extends Node
 
 @export_category("ColisionCalculator")
-@export var calculated_body: PhysicsBody3D
+## Target to which the damage will be dealt
+@export var damageable: Damageable
+## Primary shape that will be taken into account
+@export var shape: CollisionShape3D
+## If true, all shapes will be taken into account when
+## calculating damage
+@export var include_other_shapes: bool = false
+
 @export_group("ColisionModifiers")
 @export var flat_damage_reduction: float = 10.0
 @export var self_damage_multiplier: float = 1.0
 @export var dealt_damage_multiplier: float = 1.0
 
+## Object which will react to collisions
+var calculated_body: PhysicsBody3D
 var pre_collision_velosity: Vector3
 
 
 func _ready() -> void:
+	calculated_body = shape.get_parent_node_3d()
 	calculated_body.set_meta("collision_damage_calculator", self)
 
-	calculated_body.body_entered.connect(_find_parent_collision)
+	calculated_body.body_shape_entered.connect(_find_parent_collision)
 	calculated_body.contact_monitor = true
 	if calculated_body.max_contacts_reported <= 0:
 		calculated_body.max_contacts_reported = 10
@@ -23,12 +33,16 @@ func _process(_delta: float) -> void:
 	pre_collision_velosity = calculated_body.linear_velocity
 
 
-func _find_parent_collision(body: Node) -> void:
-	var damage: float = calculate_damage(calculated_body, body)
-	calculated_body.take_damage(damage)
+func _find_parent_collision(
+	_body_rid: RID, body: Node, _body_shape_index: int, local_shape_index: int
+) -> void:
+	if not include_other_shapes and calculated_body.get_child(local_shape_index) != shape:
+		return
+	var damage: Damage = calculate_damage(calculated_body, body)
+	damageable.take_damage(damage, body)
 
 
-func calculate_damage(me: Node, oponent: Node) -> float:
+func calculate_damage(me: Node, oponent: Node) -> Damage:
 	var oponet_calculator: Node
 	if oponent.has_meta("collision_damage_calculator"):
 		oponet_calculator = oponent.get_meta("collision_damage_calculator")
@@ -52,12 +66,12 @@ func calculate_damage(me: Node, oponent: Node) -> float:
 		self_dmg_mult *= oponet_calculator.dealt_damage_multiplier
 
 	# return result
-	var damage: float = base_damage * self_dmg_mult
+	var damage: Damage = Damage.new(base_damage * self_dmg_mult)
 	if "mass" in oponent:
-		damage *= oponent.mass / (me.mass + oponent.mass)
+		damage.value *= oponent.mass / (me.mass + oponent.mass)
 
-	damage -= flat_damage_reduction
-	if damage <= 0:
-		damage = 0
+	damage.value -= flat_damage_reduction
+	if damage.value <= 0:
+		damage.value = 0
 	print(damage)
 	return damage

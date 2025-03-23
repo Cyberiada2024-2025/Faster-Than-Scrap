bb05e62a2889b2b2979de2d214e68da468e3a944
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-23 11:20:03 +0100

    autoformatter

--- faster-than-scrap/code/map_select/nodes/map_node.gd
+++ faster-than-scrap/code/map_select/nodes/map_node.gd
@@ -19,6 +19,7 @@ var selected: bool = false
 var angle: float = 0
 var arc_count: int = 10
 
+
 func _process(_delta: float) -> void:
 	queue_redraw()
 	if Engine.is_editor_hint():
@@ -32,21 +33,21 @@ func _draw() -> void:
 	if selected:
 		draw_selected()
 
+
 func _draw_connections() -> void:
 	var local_start = get_rect().get_center() - get_rect().position
 	for next_node in next_map_nodes:
 		if next_node != null:
 			var offset = (
 				# offset between beggings of rects
-				Vector2(next_node.global_position.x - global_position.x,0)
+				Vector2(next_node.global_position.x - global_position.x, 0)
 				# offset to center of next node
 				+ (next_node.get_rect().get_center() - next_node.get_rect().position)
-				+ next_node.get_rect().position - get_rect().position
-				)
-			draw_line(
-				local_start,
-				offset,
-				Color.BLACK, 8.0)
+				+ next_node.get_rect().position
+				- get_rect().position
+			)
+			draw_line(local_start, offset, Color.BLACK, 8.0)
+
 
 ## change color according to node type
 func _set_color() -> void:
@@ -58,6 +59,7 @@ func _set_color() -> void:
 		modulate = Color.GRAY
 		draw_finished()
 
+
 func draw_finished():
 	var local_start = get_rect().get_center() - get_rect().position
 	var radius = get_rect().size.x / 2
@@ -66,13 +68,18 @@ func draw_finished():
 	draw_line(
 		local_start + Vector2(-radius, -radius),
 		local_start + Vector2(radius, radius),
-		Color.RED, 8.0)
+		Color.RED,
+		8.0
+	)
 
 	# draw \ line
 	draw_line(
 		local_start + Vector2(-radius, radius),
 		local_start + Vector2(radius, -radius),
-		Color.RED, 8.0)
+		Color.RED,
+		8.0
+	)
+
 
 func draw_active():
 	var local_start = get_rect().get_center() - get_rect().position
@@ -80,7 +87,7 @@ func draw_active():
 
 	var delta_angle = PI / arc_count
 	for i in range(arc_count):
-		var start_angle = 2 * PI * (i) /arc_count
+		var start_angle = 2 * PI * (i) / arc_count
 
 		draw_arc(
 			local_start,
@@ -92,39 +99,38 @@ func draw_active():
 			8
 		)
 
+
 func draw_selected():
 	var local_start = get_rect().get_center() - get_rect().position
 	var radius = (get_rect().size.x / 2) * (3.0 / 4)
 
 	var delta_angle = PI / arc_count
 
-	draw_arc(
-		local_start,
-		radius,
-		angle,
-		2*PI + angle,
-		5,
-		Color.GRAY,
-		8
-	)
+	draw_arc(local_start, radius, angle, 2 * PI + angle, 5, Color.GRAY, 8)
+
 
 func on_click() -> void:
 	clicked.emit(self)
 
+
 func on_hover_enter() -> void:
 	pass
+
 
 func on_hover_leave() -> void:
 	pass
 
+
 func get_description() -> String:
 	return ""
+
 
 func is_after_node(before_node: MapNode) -> bool:
 	for node in before_node.next_map_nodes:
 		if node == self:
 			return true
 	return false
+
 
 func change_scene(_scene_loader: SceneLoader) -> void:
 	pass
--- faster-than-scrap/code/map_select/nodes/mission_node.gd
+++ faster-than-scrap/code/map_select/nodes/mission_node.gd
@@ -5,6 +5,7 @@ extends MapNode
 
 @export var mission_info: MissionInfo
 
+
 func _set_color() -> void:
 	if Engine.is_editor_hint():
 		return
@@ -18,6 +19,8 @@ func _set_color() -> void:
 func get_description() -> String:
 	return mission_info.get_node_description()
 
+
 func change_scene(scene_loader: SceneLoader) -> void:
 	super(scene_loader)
 	scene_loader.load_fly_ship_scene()
+	mission_info.start()

0ef9c55288d3b47957bfcead3545ca927cb02070
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-29 20:05:42 +0100

    fix explodable module

--- faster-than-scrap/code/ship/modules/explodable.gd
+++ faster-than-scrap/code/ship/modules/explodable.gd
@@ -2,18 +2,17 @@ class_name ExplodableModule
 
 extends Module
 
-
-## it destroys all direct children modules
+## it destroys all direct children modules when it is destroyed
 
 
 ## Destroy self and detach children
 func _on_destroy() -> void:
 	_explode()
-	for child in self.get_children():
+	var scene_root = get_tree().current_scene
+	for child in child_modules:
 		# second check is needed for weak points as
 		# they may have been already detached
-		if child is Module and child.get_parent() == self:
-			remove_child(child)  # detach from node tree
-			get_tree().get_root().add_child(child)  # attach to scene root
+		if is_instance_valid(child) and child.get_parent() != scene_root:
+			child.reparent(scene_root)
 			child._on_destroy()
 	queue_free()  # delete self as an object

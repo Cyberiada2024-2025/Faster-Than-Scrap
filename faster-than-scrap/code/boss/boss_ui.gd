eb994fc5c644d97d27bca71306eaaecbeae35bf3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-29 22:52:03 +0100

    spawn boss

--- faster-than-scrap/code/boss/boss_ui.gd
+++ faster-than-scrap/code/boss/boss_ui.gd
@@ -5,8 +5,19 @@ extends Control
 @export var healh_bar: ProgressBar
 @export var boss_name: Label
 
+var boss: Boss
+
+
 func _ready() -> void:
-	boss_name.text = Boss.actual_boss.get_parent_node_3d().name
+	_get_boss_name.call_deferred()
+
 
 func _process(delta: float) -> void:
-	healh_bar.value = Boss.actual_boss.get_health_percentage()
+	if boss != null:
+		healh_bar.value = boss.get_health_percentage()
+
+
+func _get_boss_name() -> void:
+	var bosses = BossSpawner.boss_spawner._bosses
+	boss = bosses[0]
+	boss_name.text = boss.get_parent_node_3d().name

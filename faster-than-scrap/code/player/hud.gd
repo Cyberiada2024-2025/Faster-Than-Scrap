3d2c49ffbb71fa5c6a0ee4ac9d9585d6f69599d6
Mikul-xyz <fmikulski@o2.pl>
2025-04-01 18:22:37 +0200

    fix

--- faster-than-scrap/code/player/hud.gd
+++ faster-than-scrap/code/player/hud.gd
@@ -24,6 +24,6 @@ func _process(_delta: float) -> void:
 		return
 	# keep camera offsets relatively to player
 	var player_ship = GameManager.player_ship
-	_main_camera.global_position = player_ship.cockpit.global_position + main_camera_offset
+	_main_camera.global_position = player_ship.global_position + main_camera_offset
 	_module_camera.global_position = player_ship.global_position + module_camera_offset
 	_minimap_camera.global_position = player_ship.global_position + minimap_camera_offset
--- faster-than-scrap/code/vfx/leave_animation.gd
+++ faster-than-scrap/code/vfx/leave_animation.gd
@@ -3,19 +3,17 @@ class_name LeavingAnimation
 extends Node3D
 
 @export_category("Animation")
-@export var leaving_body: Node3D
 @export var prepare_particles: GPUParticles3D
 @export var anim_player: AnimationPlayer
 @export_group("animation names")
-@export var prepere_anim_name: StringName = "prepere_to_jump"
+@export var prepare_anim_name: StringName = "prepare_to_jump"
 @export var jump_anim_name: StringName = "jump"
 @export_group("animation times")
-@export var prepere_time: float = 4.0
+@export var prepare_time: float = 4.0
 @export var jump_time: float = 2.0
 
+## variable that block multiple activation of this animation
 var ending: Callable
-var position_base: Vector3
-var last_time: float = 0
 var callable: bool = true
 
 
@@ -27,11 +25,11 @@ func start_animation(ending_method: Callable) -> void:
 	if callable:
 		callable = false
 		ending = ending_method
-		anim_player.play(prepere_anim_name, -1, 1 / prepere_time)
+		anim_player.play(prepare_anim_name, -1, 1 / prepare_time)
 
 
 func _on_animation_finished(anim_name: StringName) -> void:
-	if anim_name == prepere_anim_name:
+	if anim_name == prepare_anim_name:
 		anim_player.play(jump_anim_name, -1, 1 / jump_time)
 	elif anim_name == jump_anim_name:
 		callable = true

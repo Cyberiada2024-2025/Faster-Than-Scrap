4e1971ad64614fa615ce7fe67bd519a641716252
Mikul-xyz <fmikulski@o2.pl>
2025-04-01 18:13:25 +0200

    pause menu

---
+++ faster-than-scrap/code/pause_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_ESCAPE):
+		pass
+
+
+func _on_resume_pressed() -> void:
+	pass  # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass  # Replace with function body.

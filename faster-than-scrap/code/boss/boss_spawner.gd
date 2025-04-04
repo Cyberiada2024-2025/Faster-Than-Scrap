dde64b2a58d9e80a725bec9164a9907005fa2137
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-04 22:23:35 +0200

    Boss script (#251)
    Created two sample bosses prefabs with scripts. Those two can be found
    in the prefabs directory.
    Bosses are made as enemies - they use state machine.
    Both bosses are modular to some extent, to show interesting behaviour of
    them.
    Boss node can be either miniboss or normal boss. After deafeting the
    miniboss the player is returned to map selector, otherwise to credits.
    
    Added also a shootable ship to test the bosses in game.
    
    To shoot press Q.
    
    
    To test it you can:
    - launch the sandbox scene (wierzba/boss_arena/boss_arena), you can even
    change the boss to spawn in the DummyBossNodeSetter node.
    - launch main_menu scene (the main scene of the game) and go through
    bottom missions and kill the boss in the middle of a game XD

---
+++ faster-than-scrap/code/boss/boss_spawner.gd
@@ -0,0 +1,44 @@
+class_name BossSpawner
+
+extends Node3D
+
+## Spawn bosses on scene load, and waits for them to die.
+## When all of the bosses are dead, the scene is loaded.
+## If it is a miniboss mission it changes the scene to map_select, otherwise to credits.
+
+static var boss_spawner: BossSpawner
+
+var _bosses: Array[Boss] = []
+var _scene_loader: SceneLoader
+
+
+func _enter_tree() -> void:
+	boss_spawner = self
+
+
+func _ready() -> void:
+	_scene_loader = SceneLoader.new()
+	add_child(_scene_loader)
+	for boss_prefab in BossManager.bosses_to_spawn:
+		_spawn_boss(boss_prefab)
+
+
+func _process(_delta: float) -> void:
+	if _all_bosses_dead():
+		if BossManager.is_miniboss:
+			_scene_loader.load_map_selector_scene()
+		else:
+			_scene_loader.load_credits_scene()
+
+
+func _spawn_boss(boss_prefab: PackedScene) -> void:
+	var boss_controller: ShipController = boss_prefab.instantiate()
+	_bosses.append(boss_controller.ship)
+	get_tree().current_scene.add_child(boss_controller)
+
+
+func _all_bosses_dead() -> bool:
+	for boss in _bosses:
+		if boss.get_health_percentage() != 0:
+			return false
+	return true

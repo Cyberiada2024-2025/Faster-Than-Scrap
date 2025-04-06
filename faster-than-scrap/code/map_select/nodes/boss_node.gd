903d810b64b2312e21efdb6f76206f2c90053fb9
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-06 14:14:45 +0200

    spawn player below boss

--- faster-than-scrap/code/map_select/nodes/boss_node.gd
+++ faster-than-scrap/code/map_select/nodes/boss_node.gd
@@ -21,4 +21,4 @@ func change_scene(scene_loader: SceneLoader) -> void:
 	super(scene_loader)
 	BossManager.bosses_to_spawn = boss_prefabs
 	BossManager.is_miniboss = is_miniboss
-	scene_loader.load_boss_scene(Vector3(0, 0, -10))
+	scene_loader.load_boss_scene(Vector3(0, 0, 15))

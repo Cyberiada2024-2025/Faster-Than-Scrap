4e76d29a0cfc3a4f007792210fb47d16b3cd49ec
Mikul-xyz <fmikulski@o2.pl>
2025-04-15 18:05:19 +0200

    linter

--- faster-than-scrap/code/spawners/triangle_entity_spawner.gd
+++ faster-than-scrap/code/spawners/triangle_entity_spawner.gd
@@ -3,9 +3,9 @@ class_name TriangleEntitySpawner
 extends BaseEntitySpawner
 
 @export_category("Triangle Spawner")
-@export var A_point: Vector3
-@export var B_point: Vector3
-@export var C_point: Vector3
+@export var a_point: Vector3
+@export var b_point: Vector3
+@export var c_point: Vector3
 
 
 #a + sqrt(randf()) * (-a + b + randf() * (c - b))
@@ -13,7 +13,7 @@ func spawn_entities():
 	for i in range(entities_count):
 		var ent: Node = spawned_entities[randi_range(0, len(spawned_entities) - 1)].instantiate()
 		ent.position = (
-			A_point + sqrt(randf()) * ((B_point - A_point) + randf() * (C_point - B_point))
+			a_point + sqrt(randf()) * ((b_point - a_point) + randf() * (c_point - b_point))
 		)
 		ent.position += (
 			Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), randf_range(0, 2 * PI))

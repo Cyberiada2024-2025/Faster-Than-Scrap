51c3e8b760ba473777b1ff14d20d095d2d6acf68
Mikul-xyz <fmikulski@o2.pl>
2025-04-15 17:52:34 +0200

    triangle spawner

--- faster-than-scrap/sandbox/mikulski/spawner_test.gd
+++ faster-than-scrap/sandbox/mikulski/spawner_test.gd
@@ -1,11 +1,24 @@
 extends Node3D
 
-@export var spawner: EntitySpawner
+@export var spawner: BaseEntitySpawner
 @export var point1: Node3D
 @export var point2: Node3D
+@export var point3: Node3D
 
 
 func _ready() -> void:
-	spawner.points[0] = point1.position
-	spawner.points[1] = point2.position
+	spawner.A_point = point1.position
+	spawner.B_point = point2.position
+	spawner.C_point = point3.position
+	spawner.entities_count = abs(
+		(
+			(
+				spawner.A_point.x * (spawner.B_point.z - spawner.C_point.z)
+				+ spawner.B_point.x * (spawner.C_point.z - spawner.A_point.z)
+				+ spawner.C_point.x * (spawner.A_point.z - spawner.B_point.z)
+			)
+			/ 400
+		)
+	)
 	spawner.spawn_entities()
+	print(spawner.entities_count)

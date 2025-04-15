71eabb0bee2f2e892d5a11e9d12cc71ba6a78398
Mikul-xyz <fmikulski@o2.pl>
2025-04-15 17:52:34 +0200

    triangle spawner

--- faster-than-scrap/code/missions/info/mission_info_get_item.gd
+++ faster-than-scrap/code/missions/info/mission_info_get_item.gd
@@ -20,5 +20,28 @@ func get_node_description() -> String:
 	return "Mission Type:\nGet item"
 
 
-func get_target_position() -> Vector3:
-	return item_position
+func get_spawner() -> BaseEntitySpawner:
+	var spawner: TriangleEntitySpawner = (
+		preload("res://prefabs/spawners/triangle_enemy_spawner.tscn").instantiate()
+	)
+	spawner.A_point = GameManager.player_ship.position
+	spawner.B_point = item_position
+	spawner.C_point = item_target_position
+	spawner.max_position_change = 10.0
+	spawner.entities_count = (
+		(
+			(2 + difficulty)
+			* abs(
+				(
+					(
+						spawner.A_point.x * (spawner.B_point.z - spawner.C_point.z)
+						+ spawner.B_point.x * (spawner.C_point.z - spawner.A_point.z)
+						+ spawner.C_point.x * (spawner.A_point.z - spawner.B_point.z)
+					)
+					/ 400
+				)
+			)
+		)
+		+ 5
+	)
+	return spawner
--- faster-than-scrap/code/spawners/triangle_entity_spawner.gd
+++ faster-than-scrap/code/spawners/triangle_entity_spawner.gd
@@ -1,16 +1,20 @@
-class_name LineEntitySpawner
+class_name TriangleEntitySpawner
 
 extends BaseEntitySpawner
 
-@export_category("Line Spawner")
-@export var start_point: Vector3
-@export var end_point: Vector3
+@export_category("Triangle Spawner")
+@export var A_point: Vector3
+@export var B_point: Vector3
+@export var C_point: Vector3
 
 
+#a + sqrt(randf()) * (-a + b + randf() * (c - b))
 func spawn_entities():
 	for i in range(entities_count):
 		var ent: Node = spawned_entities[randi_range(0, len(spawned_entities) - 1)].instantiate()
-		ent.position = (start_point + randf() * (end_point - start_point))
+		ent.position = (
+			A_point + sqrt(randf()) * ((B_point - A_point) + randf() * (C_point - B_point))
+		)
 		ent.position += (
 			Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), randf_range(0, 2 * PI))
 			* randf_range(0, max_position_change)

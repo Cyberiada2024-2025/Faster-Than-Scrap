eadbcf97863ca355da6b35f28afd77d0b91594c3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-19 20:23:09 +0200

    Add non definite state
    A state which informs the transition that it has finished. Should be used rarely!!!!

---
+++ faster-than-scrap/code/enemy/transitions/transition_on_state_finish.gd
@@ -0,0 +1,12 @@
+class_name TransitionOnStateFinish
+extends baseTransition
+
+## This class should not be overused! It is supposed to be used in states, which
+## are hard to determine how long should the last. For example the state
+## (move to position) should change when it reach the destination. It can take
+## different each time, or the destination will be different each time.
+## Thus, for this instances this transition could be used.
+
+
+func _relay() -> void:
+	finished.emit()

21c0129c777d630979727e23065c968621a445c3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-22 00:31:49 +0200

    fix test scene

--- faster-than-scrap/sandbox/wierzba/popups/popup_spawner.gd
+++ faster-than-scrap/sandbox/wierzba/popups/popup_spawner.gd
@@ -3,9 +3,9 @@ extends Node
 
 func _ready():
 	# show first popup
-	await PopupInfo._show_popup("User manual", "Please confirm")
+	await PopupInfo.show_popup("User manual", "Please confirm")
 	print("User manual confirmed")
 
 	# then second
-	var selection = await PopupRadio._show_popup("Radio!!", "Wybierz liczbę!", ["1", "2", "3"])
+	var selection = await PopupRadio.show_popup("Radio!!", "Wybierz liczbę!", ["1", "2", "3"])
 	print("Selected: " + str(selection + 1))

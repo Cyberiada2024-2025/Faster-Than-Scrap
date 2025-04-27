341adb65e443209ccce904c898f2dc47e9c09fd7
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-27 23:03:53 +0200

    implement slide

--- faster-than-scrap/code/ui/cutscenes/slide.gd
+++ faster-than-scrap/code/ui/cutscenes/slide.gd
@@ -1,14 +1,40 @@
 class_name Slide
-extends Node
+extends ColorRect
 
-## Base class for all slides. It hase 
+## Base class for all slides.
+
+signal started
 
 @export var duration: float = 1
 
+const black_transparent = Color(0, 0, 0, 0)
+const black_non_transparent = Color(0, 0, 0, 1)
+
+
+func _enter_tree() -> void:
+	color = black_non_transparent
+	z_index = 1  # make sure it's in front
+	for child: CanvasItem in get_children():
+		child.z_index = -1  # make sure it's behind
+
+	self.hide()
+
 
 func start_slide() -> void:
-	pass
+	started.emit()
+	_reveal()
 
 
-func hide_slide() -> void:
-	pass
+func _reveal() -> void:
+	self.show()
+	var tween = get_tree().create_tween()
+	# Tween color over 1 second
+	tween.tween_property(self, "color", black_transparent, 1.0)
+	await tween.finished
+
+
+func _hide() -> void:
+	var tween = get_tree().create_tween()
+	# Tween color over 1 second
+	tween.tween_property(self, "color", black_non_transparent, 1.0)
+	await tween.finished

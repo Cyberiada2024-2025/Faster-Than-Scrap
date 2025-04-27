bc8ff21eca8d8fb37bbb7365a1cf242e424e834f
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-27 19:16:41 +0200

    Create ideas for slides

---
+++ faster-than-scrap/code/ui/cutscenes/cutscene.gd
@@ -0,0 +1,38 @@
+class_name Cutscene
+extends ColorRect
+
+## Cutscene stops the game to show itself and unpauses the game when finished.
+## It consists of slides (each slide is a node), which are children of this object.
+## It reveals them one by one.
+
+var slides: Array[Slide]
+
+const black_transparent = Color(0, 0, 0, 0)
+const black_non_transparent = Color(0, 0, 0, 1)
+
+
+func _enter_tree() -> void:
+	slides.assign(find_children("*", "Slide"))
+	color = black_transparent
+
+	play()
+
+
+func play() -> void:
+	await _shine_bright()
+
+	GameManager._pause_entities()
+
+	# play all slides
+	for slide in slides:
+		await slide.start_slide()
+
+	GameManager._unpause_entities()
+
+
+## before pausing the game shine the screen bright
+func _shine_bright() -> void:  # like a diamond
+	var tween = get_tree().create_tween()
+	# Tween color over 1 second
+	tween.tween_property(self, "color", black_non_transparent, 1.0)
+	await tween.finished
---
+++ faster-than-scrap/code/ui/cutscenes/slide.gd
@@ -0,0 +1,14 @@
+class_name Slide
+extends Node
+
+## Base class for all slides. It hase 
+
+@export var duration: float = 1
+
+
+func start_slide() -> void:
+	pass
+
+
+func hide_slide() -> void:
+	pass

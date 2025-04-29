9e935039f3f7dad49445ee6b74f9b30f36027792
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-04-29 18:01:55 +0200

    Saving the fly scene and player position when entering/exiting the shop (#369)
    Resolves #282
    
    Also added a cooldown to the shop trigger, so the player doesn't
    immediately go back after exiting.
    (The cooldown only affects the body_entered event, so staying in the
    shop trigger will not teleport the player to the shop after some time -
    you need to exit the trigger and enter it again to return to the shop)

--- faster-than-scrap/code/evironment/player_detector.gd
+++ faster-than-scrap/code/evironment/player_detector.gd
@@ -4,11 +4,25 @@ extends Area3D
 
 signal player_entered
 
+## The player detector will not emit any signals for this number of seconds after entering the tree,
+## regardless of whether the player has entered the tree or not.
+@export var instantiated_cooldown: float = 0
+
+var _time_instantiated: float
+
+
+func _enter_tree():
+	_time_instantiated = Time.get_unix_time_from_system()
+
 
 func _ready() -> void:
 	body_entered.connect(_check_if_player)
 
 
 func _check_if_player(body: Node3D) -> void:
+	var current_time = Time.get_unix_time_from_system()
+	if current_time - _time_instantiated < instantiated_cooldown:
+		return
+
 	if body == GameManager.player_ship:
 		player_entered.emit()

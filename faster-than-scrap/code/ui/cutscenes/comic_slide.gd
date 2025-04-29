10de3188c43ae3c9bf2cfa6268de0c8c133b5ac6
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-29 10:34:29 +0200

    implement comic slide

--- faster-than-scrap/code/ui/cutscenes/comic_slide.gd
+++ faster-than-scrap/code/ui/cutscenes/comic_slide.gd
@@ -1,1 +1,30 @@
-extends Node
+class_name ComicSlide
+extends Slide
+
+@export var rects: Array[ColorRect] = []
+
+var time_per_comic: float
+
+
+func _enter_tree() -> void:
+	super()
+	time_per_comic = duration / rects.size()
+	for rect: ColorRect in rects:
+		rect.color = black_non_transparent
+
+
+func play_slide() -> void:
+	started.emit()
+	await _reveal()
+	for rect: ColorRect in rects:
+		_reveal_comic_part(rect)
+		await get_tree().create_timer(time_per_comic, true).timeout
+	await _hide_tween()
+
+
+func _reveal_comic_part(rect: ColorRect) -> void:
+	show()
+	var tween := get_tree().create_tween().bind_node(self)
+	# Tween color over 1 second
+	tween.tween_property(rect, "color", black_transparent, time_per_comic / 2)
+	await tween.finished

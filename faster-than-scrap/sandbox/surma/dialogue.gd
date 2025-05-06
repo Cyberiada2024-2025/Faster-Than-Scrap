46fe1ecb9ade3d26df5d8deab4fb0ca555063216
Executed419 <lucker10@interia.pl>
2025-05-06 22:32:37 +0200

    linterro

--- faster-than-scrap/sandbox/surma/dialogue.gd
+++ faster-than-scrap/sandbox/surma/dialogue.gd
@@ -4,26 +4,17 @@ class_name Dialogue extends Node
 @export var radio_choices: Array[String]
 @export var radio_results: Array[Dialogue]
 @export var next_dialogue: Dialogue
-
-enum Type { info, radio }
 @export var type: Type
-
-#func _ready() -> void:
-#if type == Type.info:
-#await PopupInfo.show_popup(title, text)
-#if type == Type.radio:
-#await PopupRadio.show_popup(title, text, radioChoices)
-#if nextDialogue != null:
-#nextDialogue.next_dialogue()
+enum Type { Info, Radio }
 
 
 func print_dialogue() -> void:
-	if type == Type.info:
+	if type == Type.Info:
 		await PopupInfo.show_popup(title, text)
 		if next_dialogue != null:
 			next_dialogue.print_dialogue()
 
-	if type == Type.radio:
+	if type == Type.Radio:
 		var selection = await PopupRadio.show_popup(title, text, radio_choices)
 		if radio_results != null:
 			radio_results[selection].print_dialogue()

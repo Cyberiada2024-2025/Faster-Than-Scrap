d4f4219c6fffcdb468cad3d51202d13e8dcef786
Executed419 <lucker10@interia.pl>
2025-05-06 14:19:49 +0200

    dialogue setup

--- faster-than-scrap/sandbox/surma/dialogue.gd
+++ faster-than-scrap/sandbox/surma/dialogue.gd
@@ -1,6 +1,25 @@
-class_name Dialogue extends Resource
+class_name Dialogue extends Node
+@export var text: String
+@export var title: String
+@export var radioChoices: Array[String]
+@export var radioResults: Array[Dialogue]
+@export var nextDialogue: Dialogue
 
-@export var textInfo: PopupInfo
-@export var textRadio: PopupRadio
- 
-var dialog: Dialogue
+enum Type { info, radio }
+@export var type: Type
+
+
+func _ready() -> void:
+	if type == Type.info:
+		await PopupInfo.show_popup(title, text)
+	if type == Type.radio:
+		await PopupRadio.show_popup(title, text, radioChoices)
+	if nextDialogue != null:
+		nextDialogue.next_dialogue()
+
+
+func next_dialogue() -> void:
+	if type == Type.info:
+		await PopupInfo.show_popup(title, text)
+	if nextDialogue != null:
+		nextDialogue.next_dialogue()

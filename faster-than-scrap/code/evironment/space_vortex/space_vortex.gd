6a7daf1a326a68507893a72fb38097c6a80d3aaf
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-08 19:51:33 +0200

    Create parametrized constructor

--- faster-than-scrap/code/evironment/space_vortex/space_vortex.gd
+++ faster-than-scrap/code/evironment/space_vortex/space_vortex.gd
@@ -7,27 +7,38 @@ const VORTEX_PREFAB: PackedScene = preload(
 const VORTEX_TIMER_PATH: String = "res://prefabs/environment/space_vortex/inside_vortex_timer.tscn"
 const TIMER_NAME: String = "VortexTimer926452"
 
-const START_SCALE: float = 100
-const MIN_SCALE: float = 0.001
-const SHRINKING_TIME: float = 60  # in second
-const SHRINK_SPEED: float = (START_SCALE - MIN_SCALE) / SHRINKING_TIME  # units per seconds
+var start_scale: float = 400
+var min_scale: float = 0.001
+var shrinking_time: float = 3 * 60  # in seconds
+var shrink_speed: float  # units per seconds
 
 
-static func spawn_vortex(start_position: Vector3) -> void:
+static func spawn_vortex(
+	start_position: Vector3,
+	start_scale: float = 400,
+	min_scale: float = 0.001,
+	shrinking_time: float = 3 * 60
+) -> void:
 	var vortex = VORTEX_PREFAB.instantiate()
+
 	vortex.position = start_position
+	vortex.start_scale = start_scale
+	vortex.min_scale = min_scale
+	vortex.shrinking_time = shrinking_time
+
 	GameManager.get_tree().current_scene.add_child.call_deferred(vortex)
 
 
 func _ready() -> void:
-	scale.x = START_SCALE
-	scale.z = START_SCALE
+	shrink_speed = (start_scale - min_scale) / shrinking_time
+	scale.x = start_scale
+	scale.z = start_scale
 
 
 func _process(delta: float) -> void:
-	scale.x -= delta * SHRINK_SPEED
-	if scale.x < MIN_SCALE:
-		scale.x = MIN_SCALE
+	scale.x -= delta * shrink_speed
+	if scale.x < min_scale:
+		scale.x = min_scale
 	scale.z = scale.x
 
 

5344040038f5a972729606684955e6d9a44947bb
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-08 18:57:35 +0200

    Shrink vortex isntead of grow

--- faster-than-scrap/code/evironment/space_vortex/space_vortex.gd
+++ faster-than-scrap/code/evironment/space_vortex/space_vortex.gd
@@ -2,23 +2,37 @@ extends Area3D
 
 const VORTEX_TIMER_PATH: String = "res://prefabs/environment/space_vortex/inside_vortex_timer.tscn"
 const TIMER_NAME: String = "VortexTimer926452"
-const ENLARGMENT_SPEED: float = 1.0  #units per seconds
+
+const START_SCALE: float = 100
+const MIN_SCALE: float = 0.001
+const SHRINKING_TIME: float = 60  # in second
+const SHRINK_SPEED: float = (START_SCALE - MIN_SCALE) / SHRINKING_TIME  # units per seconds
+
+
+static func spawn_vortex(start_position: Vector3) -> void:
+	pass
+
+
+func _ready() -> void:
+	scale.x = START_SCALE
+	scale.z = START_SCALE
 
 
 func _process(delta: float) -> void:
-	self.scale.x += delta * ENLARGMENT_SPEED
-	self.scale.y += delta * ENLARGMENT_SPEED
-	self.scale.z += delta * ENLARGMENT_SPEED
+	scale.x -= delta * SHRINK_SPEED
+	if scale.x < MIN_SCALE:
+		scale.x = MIN_SCALE
+	scale.z = scale.x
 
 
-func _on_body_entered(body: Node3D) -> void:
+func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group("affected by vortex"):
 		var timer = preload(VORTEX_TIMER_PATH).instantiate()
 		timer.name = TIMER_NAME
 		body.add_child(timer)
 
 
-func _on_body_exited(body: Node3D) -> void:
+func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("affected by vortex"):
 		for child in body.get_children():
 			if child.name == TIMER_NAME:

4359eb5f61385a972348703fb43668909e348a5f
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-12 00:49:04 +0200

    Fix look at script (rebase error?)

--- faster-than-scrap/code/enemy/states/rotate/look_at_target_with_object.gd
+++ faster-than-scrap/code/enemy/states/rotate/look_at_target_with_object.gd
@@ -9,12 +9,13 @@ var rotation_offset: float = 0
 
 func enter(_previous_state_path: String, _data := {}) -> void:
 	target = GameManager.find_closest_ship(ship_controller.ship)
+	look_object = null
 
 
 ## Called by the state machine on the engine's main loop tick.
 func state_physics_update(_delta: float) -> void:
 	if not is_instance_valid(look_object):
-		look_object = _select_look_object()
+		_select_look_object()
 
 	# calculate target rotation
 	var look_direction = self.target.global_position - self.ship_controller.global_position
@@ -25,10 +26,9 @@ func state_physics_update(_delta: float) -> void:
 
 
 ## select the object having the smallest angle resulted from the ship rotation
-func _select_look_object() -> Node3D:
+func _select_look_object() -> void:
 	var index: int = 0
 	var smallest_angle: float = INF
-	var best_index: int = 0
 
 	# check if any was deleted
 	for object in objects:
@@ -41,22 +41,27 @@ func _select_look_object() -> Node3D:
 
 		if angle < smallest_angle:
 			smallest_angle = angle
-			best_index = index
+			look_object = object
 		index += 1
 
 	# check if empty
 	if objects.size() == 0:
-		return null
+		rotation_offset = 0
+		look_object = null
+		return
 
 	# calculate rotation offset from look object
-	var rotation_offset = 0
 	if look_object != null:
 		_set_rotation_offset(look_object)
-
-	return objects[best_index]
 
 
 func _set_rotation_offset(look_object: Node3D) -> void:
 	var ship_forward = -ship_controller.global_transform.basis.z  # in godot forward is [0,0,-1]
 	var look_foward = -look_object.global_transform.basis.z  # in godot forward is [0,0,-1]
-	rotation_offset = ship_forward.angle_to(look_foward)
+
+	# cast to 2d, to get signed angle_to call
+	var ship_forward_2d = Vector2(ship_forward.x, ship_forward.z)
+	var look_foward_2d = Vector2(look_foward.x, look_foward.z)
+
+	rotation_offset = ship_forward_2d.angle_to(look_foward_2d)
+	pass

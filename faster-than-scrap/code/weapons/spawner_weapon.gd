91703c9f2e30a50fc152f42ab14ac824cad47c86
PawelSzczepan <szczeppawel@gmail.com>
2025-05-11 19:01:08 +0200

    linter and fixes

--- faster-than-scrap/code/weapons/spawner_weapon.gd
+++ faster-than-scrap/code/weapons/spawner_weapon.gd
@@ -17,9 +17,9 @@ func try_activate() -> Node3D:
 	var new_projectile = _spawn_projectile()
 	new_projectile.transform = global_transform
 	get_tree().current_scene.add_child(new_projectile)
-	
+
 	if muzzle_flash != null:
 		muzzle_flash.restart()
 		muzzle_flash.emitting = true
-	
+
 	return new_projectile

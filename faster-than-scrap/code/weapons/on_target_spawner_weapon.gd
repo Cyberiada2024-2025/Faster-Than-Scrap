2bc20ccbd5db732478033e8ac28c0a7d92196ac5
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-12 23:43:32 +0200

    Add option to rotate spike to target

--- faster-than-scrap/code/weapons/on_target_spawner_weapon.gd
+++ faster-than-scrap/code/weapons/on_target_spawner_weapon.gd
@@ -3,6 +3,9 @@ extends SpawnerWeapon
 
 @export var y_offset: float = 0
 
+@export_category("rotate towards target")
+@export var base_radius: float = 0
+
 
 func try_activate() -> Node3D:
 	var created_projectile = super()
@@ -14,5 +17,10 @@ func try_activate() -> Node3D:
 		return null
 	created_projectile.global_position = target.global_position
 	created_projectile.position.y += y_offset
+
+	var offset_2d = RandomUtils._random_on_edge_unit_circle() * base_radius
+	created_projectile.global_position.x += offset_2d.x
+	created_projectile.global_position.z += offset_2d.y
+	created_projectile.look_at(target.global_position)
 
 	return created_projectile

2d3de018e47645ac66bf1afe3df0660813ea557f
Vertical-Edek <157529422+Vertical-Edek@users.noreply.github.com>
2025-05-13 21:17:09 +0200

    Music added

--- faster-than-scrap/code/utilities/wait_free.gd
+++ faster-than-scrap/code/utilities/wait_free.gd
@@ -5,5 +5,5 @@ extends Node3D
 @export var wait: float = 1.0
 
 func wait_free() -> void:
-	await get_tree().create_timer(wait).timeout
+dd	await get_tree().create_timer(wait).timeout
 	queue_free()

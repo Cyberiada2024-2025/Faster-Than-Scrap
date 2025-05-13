503f2ec4b518d1cf503e04efa6610ef03dc8478e
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-13 09:15:14 +0200

    Fix WaitFree
    get_tree() is null for orphans

--- faster-than-scrap/code/utilities/wait_free.gd
+++ faster-than-scrap/code/utilities/wait_free.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 @export var wait: float = 1.0
 
+
 func wait_free() -> void:
-	await get_tree().create_timer(wait).timeout
+	await GameManager.get_tree().create_timer(wait).timeout
 	queue_free()

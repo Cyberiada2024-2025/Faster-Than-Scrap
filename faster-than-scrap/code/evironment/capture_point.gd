18d1fa37ba34607a04a8687780872f92b57ad095
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-15 18:49:07 +0200

    Capture point script

---
+++ faster-than-scrap/code/evironment/capture_point.gd
@@ -0,0 +1,40 @@
+class_name CapturePoint
+extends Area3D
+
+signal on_capture
+
+## The player detector will not emit any signals for this number of seconds after entering the tree,
+## regardless of whether the player has entered the tree or not.
+@export var instantiated_cooldown: float = 0
+@export var capture_time: float = 2
+
+var _player_in_range: bool = false
+var _capture_counter: float = capture_time
+var _captured: bool = false
+
+
+func _ready() -> void:
+	body_entered.connect(_check_if_player_entered)
+	body_exited.connect(_check_if_player_exited)
+
+
+func _check_if_player_entered(body: Node3D) -> void:
+	if body == GameManager.player_ship:
+		_player_in_range = true
+
+
+func _check_if_player_exited(body: Node3D) -> void:
+	if body == GameManager.player_ship:
+		_player_in_range = false
+		_capture_counter = capture_time
+
+
+func _process(delta: float) -> void:
+	if _captured:
+		return
+
+	if _player_in_range:
+		_capture_counter -= delta
+		if _capture_counter <= 0:
+			_captured = true
+			on_capture.emit()

af452b737764040ae1c0db2393c5270f18a252f3
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-05-15 20:15:17 +0200

    Added energy regeneration change

--- faster-than-scrap/code/ship/modules/battery_module.gd
+++ faster-than-scrap/code/ship/modules/battery_module.gd
@@ -3,15 +3,18 @@ class_name BatteryModule
 extends Module
 
 @export var energy: float = 25
+@export var energy_regeneration: float = 5
 
 
 func detach() -> void:
 	if ship != null:
 		ship.max_energy -= energy
+		ship.restore -= energy_regeneration
 	super()
 
 
 func attach() -> void:
 	if ship != null:
 		ship.max_energy += energy
+		ship.restore += energy_regeneration
 	super()

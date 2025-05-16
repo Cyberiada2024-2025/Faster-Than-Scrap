8b47e66d02611eb48a83a789e30aec4846f7e885
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-16 22:49:45 +0200

    Fix droppable spawning position

--- faster-than-scrap/code/evironment/droppable.gd
+++ faster-than-scrap/code/evironment/droppable.gd
@@ -3,12 +3,26 @@ extends Node3D
 ## When this node is deleted, the new object will spawn and
 ## added to the root of the scene.
 
+## Due to the lifecycle of a Node, the global position is saved each frame.
+## When notification of deletion arrives, the node is already not in a scene tree, so
+## the global_position is 0, thus the global_position needs saving.
+
 @export var object_to_spawn: PackedScene
+
+var last_global_pos: Vector3
+
+
+func _process(_delta: float) -> void:
+	last_global_pos = global_position
 
 
 func _notification(notification: int) -> void:
 	if notification == NOTIFICATION_PREDELETE:
-		var object: Node3D = object_to_spawn.instantiate()
-		object.global_position = global_position
+		_drop()
 
-		GameManager.get_tree().current_scene.add_child.call_deferred(object)
+
+func _drop() -> void:
+	var object: Pickable = object_to_spawn.instantiate()
+
+	GameManager.get_tree().current_scene.add_child.call_deferred(object)
+	object.global_position = last_global_pos

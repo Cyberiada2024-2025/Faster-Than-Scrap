06d82d4139cb16f2b1e78c0dc5a1f354b6595fa1
PawelSzczepan <szczeppawel@gmail.com>
2025-05-19 16:58:34 +0200

    linter

--- faster-than-scrap/code/weapons/projectiles/beam_warning.gd
+++ faster-than-scrap/code/weapons/projectiles/beam_warning.gd
@@ -1,6 +1,6 @@
 extends SpawnerWeapon
 
-const anim: String = "Flash"
+const ANIM: String = "Flash"
 
 @export var warning_time: float = 2.0
 @export var animator: AnimationPlayer
@@ -10,10 +10,10 @@ var active_projectile: Node3D
 func _ready() -> void:
 	# wait one frame, so weapon can get us in position
 	await get_tree().process_frame
-	animator.play(anim, -1, 1/warning_time)
-	
+	animator.play(ANIM, -1, 1/warning_time)
+
 	await get_tree().create_timer(warning_time).timeout
-	
+
 	active_projectile = _spawn_projectile()
 	add_child(active_projectile)
 	if muzzle_flash != null:
--- faster-than-scrap/code/weapons/projectiles/projectile_warning.gd
+++ faster-than-scrap/code/weapons/projectiles/projectile_warning.gd
@@ -1,6 +1,6 @@
 extends SpawnerWeapon
 
-const anim: String = "Flash"
+const ANIM: String = "Flash"
 
 @export var warning_time: float = 2.0
 @export var animator: AnimationPlayer
@@ -12,17 +12,17 @@ const anim: String = "Flash"
 func _ready() -> void:
 	# wait one frame, so weapon can get us in position
 	await get_tree().process_frame
-	animator.play(anim, -1.0, 1.0/warning_time)
-	
+	animator.play(ANIM, -1.0, 1.0/warning_time)
+
 	# puts graphics on y=0 plane at point we are looking at
 	# this is for vortex spike attack really
 	# no I don't know why /5 has to be there
 	if zero_y_pos && graphics != null:
 		graphics.position.z = -abs(position.y)/sin(PI/2-basis.get_euler().x)/5
 		graphics.global_rotation = Vector3.ZERO
-	
+
 	await get_tree().create_timer(warning_time).timeout
-	
+
 	var new_projectile = _spawn_projectile()
 	new_projectile.transform = global_transform
 	get_tree().current_scene.add_child(new_projectile)
@@ -30,5 +30,5 @@ func _ready() -> void:
 	if muzzle_flash != null:
 		muzzle_flash.restart()
 		muzzle_flash.emitting = true
-		
+
 	queue_free()

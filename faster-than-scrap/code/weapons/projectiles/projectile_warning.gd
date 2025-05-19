d4b60343cc0c56eef120594be15ac128eb1dba48
Paweł Szczepański <107117250+PawelSzczepan@users.noreply.github.com>
2025-05-19 18:08:45 +0200

    Warnings (#410)
    Warnings added to boss attacks.
    Made minor changes to boss behaviour so bosses wait until the actually
    shoot.
    Warning are made as a wrapper around projectile, shot like a projectile.
    I really didn't have any idea for bullet warning so red sparkles it is.

---
+++ faster-than-scrap/code/weapons/projectiles/projectile_warning.gd
@@ -0,0 +1,40 @@
+extends SpawnerWeapon
+
+const ANIM: String = "Flash"
+
+@export var warning_time: float = 2.0
+@export var animator: AnimationPlayer
+
+@export var zero_y_pos: bool
+@export var graphics: Node3D
+
+
+func _ready() -> void:
+	# wait one frame, so weapon can get us in position
+	await get_tree().process_frame
+	animator.play(ANIM, -1.0, 1.0/warning_time)
+
+	# puts graphics on y=0 plane at point we are looking at
+	# this is for vortex spike attack really
+	# no I don't know why /5 has to be there
+	# y=0------------o---------
+	#           |   /
+	# position.y|  /-graphics.z
+	#           | /
+	#           x--)basis.get_euler().x
+	# we move along local -z axis by sin(90-basis.get_euler().x [degrees])
+	if zero_y_pos && graphics != null:
+		graphics.position.z = -abs(position.y)/sin(PI/2-basis.get_euler().x)/5
+		graphics.global_rotation = Vector3.ZERO
+
+	await get_tree().create_timer(warning_time).timeout
+
+	var new_projectile = _spawn_projectile()
+	new_projectile.transform = global_transform
+	get_tree().current_scene.add_child(new_projectile)
+
+	if muzzle_flash != null:
+		muzzle_flash.restart()
+		muzzle_flash.emitting = true
+
+	queue_free()

782ea95213cec2806c44a0b9a640fe97d548d6c5
PawelSzczepan <szczeppawel@gmail.com>
2025-05-19 16:48:25 +0200

    boss warnings

---
+++ faster-than-scrap/code/weapons/projectiles/beam_warning.gd
@@ -0,0 +1,24 @@
+extends SpawnerWeapon
+
+const anim: String = "Flash"
+
+@export var warning_time: float = 2.0
+@export var animator: AnimationPlayer
+
+var active_projectile: Node3D
+
+func _ready() -> void:
+	# wait one frame, so weapon can get us in position
+	await get_tree().process_frame
+	animator.play(anim, -1, 1/warning_time)
+	
+	await get_tree().create_timer(warning_time).timeout
+	
+	active_projectile = _spawn_projectile()
+	add_child(active_projectile)
+	if muzzle_flash != null:
+		muzzle_flash.emitting = true
+
+func _notification(notification):
+	if (notification == NOTIFICATION_PREDELETE):
+		active_projectile.queue_free()

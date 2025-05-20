19f717adc67fb753e44caafa70529071f69dc4a2
Mikul-xyz <fmikulski@o2.pl>
2025-05-20 16:54:46 +0200

    linter

--- faster-than-scrap/code/weapons/projectiles/beam.gd
+++ faster-than-scrap/code/weapons/projectiles/beam.gd
@@ -10,15 +10,15 @@ extends Node3D
 
 var _beam_length: float
 var _was_colliding: bool = false
-var beam_particles: Node3D
+var _beam_particles: Node3D
 
 @onready var _damage_raycast: DamageRaycast3D = $DamageRaycast3D
 
 
 func _ready() -> void:
 	_beam_length = _damage_raycast.target_position.length()
-	beam_particles = preload("res://prefabs/vfx/particles/beam_particles.tscn").instantiate()
-	self.add_child(beam_particles)
+	_beam_particles = preload("res://prefabs/vfx/particles/beam_particles.tscn").instantiate()
+	self.add_child(_beam_particles)
 
 
 func _process(_delta: float) -> void:
@@ -27,13 +27,13 @@ func _process(_delta: float) -> void:
 			_damage_raycast.get_collision_point()
 		)
 		beam_indicator.scale.z = ray_length
-		beam_particles.global_position = _damage_raycast.get_collision_point()
-		beam_particles.look_at(_damage_raycast.global_position)
+		_beam_particles.global_position = _damage_raycast.get_collision_point()
+		_beam_particles.look_at(_damage_raycast.global_position)
 		if not _was_colliding:
 			_was_colliding = true
-			beam_particles.visible = true
+			_beam_particles.visible = true
 	else:
 		beam_indicator.scale.z = _beam_length
 		if _was_colliding:
 			_was_colliding = false
-			beam_particles.visible = false
+			_beam_particles.visible = false

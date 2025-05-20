7b7de815a84c421030ff7dd97f69ad0ccabdb468
Mikul-xyz <fmikulski@o2.pl>
2025-05-20 16:48:47 +0200

    beam

--- faster-than-scrap/code/weapons/projectiles/beam.gd
+++ faster-than-scrap/code/weapons/projectiles/beam.gd
@@ -9,12 +9,16 @@ extends Node3D
 @export var beam_indicator: Node3D
 
 var _beam_length: float
+var _was_colliding: bool = false
+var beam_particles: Node3D
 
 @onready var _damage_raycast: DamageRaycast3D = $DamageRaycast3D
 
 
 func _ready() -> void:
 	_beam_length = _damage_raycast.target_position.length()
+	beam_particles = preload("res://prefabs/vfx/particles/beam_particles.tscn").instantiate()
+	self.add_child(beam_particles)
 
 
 func _process(_delta: float) -> void:
@@ -23,5 +27,13 @@ func _process(_delta: float) -> void:
 			_damage_raycast.get_collision_point()
 		)
 		beam_indicator.scale.z = ray_length
+		beam_particles.global_position = _damage_raycast.get_collision_point()
+		beam_particles.look_at(_damage_raycast.global_position)
+		if not _was_colliding:
+			_was_colliding = true
+			beam_particles.visible = true
 	else:
 		beam_indicator.scale.z = _beam_length
+		if _was_colliding:
+			_was_colliding = false
+			beam_particles.visible = false

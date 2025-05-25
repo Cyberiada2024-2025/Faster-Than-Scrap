5b10ff5e306e0eac3d4c868814dc1e999897b60c
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-25 12:03:02 +0200

    Create asteroids prefabs

--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -4,12 +4,17 @@ extends RigidBody3D
 @export_category("Stats")
 @export var hp: int = 100
 @export_group("StartValues")
-@export var start_speed_range: float = 10.0
+@export var start_speed_range: float = 1
+@export var start_angular_speed_range: float = 1.0
 
 
 func _ready() -> void:
 	self.linear_velocity.x += randf_range(-start_speed_range, start_speed_range)
 	self.linear_velocity.z += randf_range(-start_speed_range, start_speed_range)
+
+	self.angular_velocity.x += randf_range(-start_angular_speed_range, start_angular_speed_range)
+	self.angular_velocity.y += randf_range(-start_angular_speed_range, start_angular_speed_range)
+	self.angular_velocity.z += randf_range(-start_angular_speed_range, start_angular_speed_range)
 
 
 func take_damage(damage: Damage) -> void:

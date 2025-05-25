4f55af57620bc6c736e1b9d17aafbf8890957a12
Vertical-Edek <157529422+Vertical-Edek@users.noreply.github.com>
2025-05-25 22:19:59 +0200

    requested changes

--- faster-than-scrap/code/main_menu/background_light.gd
+++ faster-than-scrap/code/main_menu/background_light.gd
@@ -1,14 +1,24 @@
 extends Sprite2D
+
+## Amplitude of [member Sprite2D.self_modulate.r] changes (best bept around 0 - 0.2)
+@export var red_modulation: float = 0
+## Amplitude of [member Sprite2D.self_modulate.g] changes (best bept around 0 - 0.2)
+@export var green_modulation: float = 0
+## Amplitude of [member Sprite2D.self_modulate.b] changes (best bept around 0 - 0.2)
+@export var blue_modulation: float = 0
+
 var fixed_global_position
 var time: float = 0
-var red: float = 0
+var base_modulate_color: Color = Color(0,0,0,0)
 
 func _ready():
 	fixed_global_position = global_position
+	base_modulate_color = self_modulate
 
 func _process(delta):
 	global_position = fixed_global_position
 	time = time + delta
 	time = fmod(time, 2 * PI)
-	red = 0.7 + 0.15 * sin(time)
-	self_modulate.r = red
+	self_modulate.r = base_modulate_color.r + red_modulation * sin(time)
+	self_modulate.g = base_modulate_color.g + green_modulation * sin(time)
+	self_modulate.b = base_modulate_color.b + blue_modulation * sin(time)

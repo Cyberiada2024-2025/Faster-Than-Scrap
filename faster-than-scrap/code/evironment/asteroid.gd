72f802b66766f148d1053e322a212e4e9f10bcb8
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-05-29 23:20:35 +0200

    Asteroid comment

--- faster-than-scrap/code/evironment/asteroid.gd
+++ faster-than-scrap/code/evironment/asteroid.gd
@@ -4,6 +4,7 @@ extends RigidBody3D
 @export_category("Stats")
 @export var hp: int = 100
 @export_group("StartValues")
+# parameters of start rotation speed, to prevent being static and dull
 @export var start_speed_range: float = 1
 @export var start_angular_speed_range: float = 1.0
 
@@ -12,6 +13,7 @@ func _ready() -> void:
 	self.linear_velocity.x += randf_range(-start_speed_range, start_speed_range)
 	self.linear_velocity.z += randf_range(-start_speed_range, start_speed_range)
 
+	# randomize rotation speed
 	self.angular_velocity.x += randf_range(-start_angular_speed_range, start_angular_speed_range)
 	self.angular_velocity.y += randf_range(-start_angular_speed_range, start_angular_speed_range)
 	self.angular_velocity.z += randf_range(-start_angular_speed_range, start_angular_speed_range)

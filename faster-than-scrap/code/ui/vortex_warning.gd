91d8b28171cb4d49655fa675b96ccdbc78a14662
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-06-10 10:14:51 +0200

    remove setting warning material in ready
    Co-authored-by: Lenetis <70149832+Lenetis@users.noreply.github.com>

--- faster-than-scrap/code/ui/vortex_warning.gd
+++ faster-than-scrap/code/ui/vortex_warning.gd
@@ -25,7 +25,6 @@ var warning_state: WarningState = WarningState.PLAYER_SAFE
 func _ready() -> void:
 	warning_objects_container.modulate.a = 0
 	danger_object.modulate.a = 0
-	_set_warning_material(warning_material)
 
 
 func _process(delta: float) -> void:

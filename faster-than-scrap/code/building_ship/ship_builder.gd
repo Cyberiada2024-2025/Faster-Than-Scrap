7a9e2b751d30f2146eeeea6553745010568b25d3
Mikul-xyz <fmikulski@o2.pl>
2025-06-16 19:50:48 +0200

    attach points

--- faster-than-scrap/code/building_ship/ship_builder.gd
+++ faster-than-scrap/code/building_ship/ship_builder.gd
@@ -93,11 +93,13 @@ func _update_lmb_state(event: InputEvent) -> void:
 
 
 func _update_attach_point_index(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			attach_point_index += 1
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			attach_point_index -= 1
+	if Input.is_key_pressed(KEY_R):
+		attach_point_index += 1
+	#if event is InputEventMouseButton and event.pressed:
+	#if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+	#attach_point_index += 1
+	#elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+	#attach_point_index -= 1
 
 
 # ----------------raycasts hits ------------------------------------

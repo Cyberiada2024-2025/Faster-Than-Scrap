0871a0323e9ce93edf38474dcee47bae05808f3c
sajmon <szymon17099@gmail.com>
2025-06-17 20:23:32 +0200

    Add debug menu singleton

---
+++ faster-than-scrap/code/debug_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+const BTN_PARENT = NodePath(
+	"ColorRect/MarginContainer/VBoxContainer2/CenterContainer/VBoxContainer"
+)
+
+
+func _ready() -> void:
+	visible = false
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_released("debug_menu") and OS.is_debug_build():
+		print("Working!")
+		toggle_menu()
+
+
+func toggle_menu() -> void:
+	if not visible:
+		GameManager._pause_entities()
+		visible = true
+		get_node(BTN_PARENT).get_child(0).grab_focus()
+	else:
+		GameManager._unpause_entities()
+		visible = false

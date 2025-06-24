e2fcdf612dc00b626b972d856124a1e4640bfc8a
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-06-24 15:22:27 +0200

    Minor comment

--- faster-than-scrap/code/boss/boss_spawner.gd
+++ faster-than-scrap/code/boss/boss_spawner.gd
@@ -34,6 +34,7 @@ func _process(_delta: float) -> void:
 		else:
 			if not _boss_dying:
 				_boss_dying = true
+				# wait for the boss death animation to finish
 				await get_tree().create_timer(8).timeout
 				GameManager.player_ship.leave_animation.start_animation(
 					_scene_loader.load_credits_scene

910a89b3ab39170aee96a025f13a93a8748db8d1
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-06-24 19:50:56 +0200

    Make shield control animations seperately

--- faster-than-scrap/code/ship/modules/Shield.gd
+++ faster-than-scrap/code/ship/modules/Shield.gd
@@ -2,7 +2,8 @@ class_name Shield
 
 extends Node3D
 
-@export var animator: AnimationPlayer
+@export var close_animator: AnimationPlayer
+@export var hit_animator: AnimationPlayer
 @export var collider: CollisionShape3D
 
 var on = false
@@ -18,7 +19,7 @@ func activate() -> void:
 
 	on = true
 	collider.disabled = false
-	animator.play(anim_close)
+	close_animator.play(anim_close)
 
 
 func deactivate() -> void:
@@ -28,8 +29,8 @@ func deactivate() -> void:
 
 	on = false
 	collider.disabled = true
-	animator.play_backwards(anim_close)
+	close_animator.play_backwards(anim_close)
 
 
 func take_damage() -> void:
-	animator.play(anim_hit)
+	hit_animator.play(anim_hit)

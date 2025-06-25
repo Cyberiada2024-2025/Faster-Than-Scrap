e340a40bb750484982480f5e463631bea191c648
Executed419 <lucker10@interia.pl>
2025-06-25 11:19:14 +0200

    cleanup

--- faster-than-scrap/code/singletons/settings_manager.gd
+++ faster-than-scrap/code/singletons/settings_manager.gd
@@ -1,13 +1,5 @@
 extends Node
 
 var brakes_enabled := false
-var air_resistance := true:
-	set(new_value):
-		air_resistance = new_value
-		var p = GameManager.player_ship
-		# if set before game is launched
-		# (assuming these settings should be available during game at all)
-		if p != null:
-			p.linear_damp = p.linear_damp_value
-			p.angular_damp = p.angular_damp_value
+var air_resistance := true
 var skip_cutscenes := false

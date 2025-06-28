940b2418cca9fb7ca162a53b9c1280366fa797d1
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-06-28 20:51:40 +0200

    Many collision bug fixes (#524)
    - Used mesh colliders for big asteroids
    - Caution: Godot complains that mesh colliders with nonstatic
    rigidbodies may behave incorrectly, but in my (very extensive) tests,
    they worked fine, so I think it should be ok?
    - Reordered colliders in big and small asteroids to be the first child
    of rigidbody
    - For some reason (not sure if that's a Godot thing or our spaghetti)
    the collisions were ignored if they were not the first child
    - Adjusted colliders in small asteroids to better fit their visuals
    - Unified self_damage_multiplier to be equal to 1 in all modules
    - This will probably need to be further balanced in the future, but
    currently the values were very extreme (e.g. thrusters taking 10x more
    collision damage than weapons). The balancing should be easier with
    everything set to 1, as a starting point
    - Added constant *10 collision damage multiplier
    - This is intended to replace the (now reset) per-module multipliers.
    Also solves the issue with asteroid collisions not doing much damage at
    all, that was raised during feedback gathering
    - Fixed an awful terrible bug with collisions not working for modules
    bought in the shop
    - Fixed bug with access to previously freed instances that happened
    sometimes during collisions, after a module was destroyed
    - Fixed collision damage sometimes being negative
    - Also the collision damage calculations are now better for elongated,
    non-round objects. Still not perfect though, because they don't take the
    objects' rotation into account, but that would require a huge rework and
    a *lot* of maths (especially since
    `PhysicsDirectBodyState3D.get_contact_impulse()` [is broken in Godot
    since 2023](https://github.com/godotengine/godot/issues/73541) lol) (the
    PR to fix this [also seems to have been waiting since
    2023](https://github.com/godotengine/godot/pull/73569))
    
    ---
    
    Some collision bugs are still present:
    - Enemies act as immovable brick walls and don't take any collision
    damage
    - Fixing this will most probably require rewriting them to be
    rigidbodies. I don't know how the enemies work, so I'd prefer someone
    more knowledgable to look into this
    - Detached modules don't take collision damage
    - The collision damage calculator is a big plate of spaghetti and fixing
    this would require a lot of work, to notify it somehow when it's module
    gets detached. The detached modules still *deal* collision damage to the
    player, so I'd say it's not worth the time and should not be noticeable
    in any way.

--- faster-than-scrap/code/collision_damage_calculator.gd
+++ faster-than-scrap/code/collision_damage_calculator.gd
@@ -1,5 +1,8 @@
 extends Node
 
+## All collision damage will be multiplied by this value
+const DAMAGE_MULTIPLIER = 10.0
+
 @export var collision_particles: PackedScene = preload(
 	"res://prefabs/vfx/particles/timed_particles/collision.tscn"
 )
@@ -24,15 +27,31 @@ var pre_collision_velocity: Vector3
 
 
 func _ready() -> void:
+	GameManager.new_game_state.connect(_on_game_manager_new_game_state)
+
+	find_calculated_body()
+
+
+func _on_game_manager_new_game_state(new_state: GameState.State):
+	if new_state == GameState.State.FLY:
+		find_calculated_body()
+
+
+func find_calculated_body():
+	if calculated_body != null:
+		calculated_body.body_shape_entered.disconnect(_handle_collision)
+	calculated_body = null
+
 	# check if parent is PhysicsBody3D, to make it work with module ghosts
 	if shape.get_parent_node_3d() is not PhysicsBody3D:
 		set_process(false)
 		return
+	set_process(true)
 
 	calculated_body = shape.get_parent_node_3d()
 	calculated_body.set_meta("collision_damage_calculator", self)
 
-	calculated_body.body_shape_entered.connect(_find_parent_collision)
+	calculated_body.body_shape_entered.connect(_handle_collision)
 	calculated_body.contact_monitor = true
 	if calculated_body.max_contacts_reported <= 0:
 		calculated_body.max_contacts_reported = 10
@@ -42,7 +61,7 @@ func _process(_delta: float) -> void:
 	pre_collision_velocity = calculated_body.linear_velocity
 
 
-func _find_parent_collision(
+func _handle_collision(
 	_body_rid: RID, body: Node, _body_shape_index: int, local_shape_index: int
 ) -> void:
 	if not include_other_shapes and calculated_body.get_child(local_shape_index) != shape:
@@ -50,19 +69,16 @@ func _find_parent_collision(
 	var damage: Damage = calculate_damage(calculated_body, body)
 	damageable.take_damage(damage, body)
 
-	if collision_particles == null:
-		return
+	if collision_particles != null:
+		_spawn_collision_particles()
 
+
+func _spawn_collision_particles() -> void:
 	var body_direct_state = PhysicsServer3D.body_get_direct_state(calculated_body.get_rid())
-	var contact_count = body_direct_state.get_contact_count()
+	var collision_parameters = _calculate_average_collision_values(body_direct_state)
 
-	var average_position = Vector3.ZERO
-	var average_normal = Vector3.ZERO
-	for i in range(contact_count):
-		average_position += body_direct_state.get_contact_collider_position(i)
-		average_normal += body_direct_state.get_contact_local_normal(i)
-	average_position /= contact_count
-	average_normal /= contact_count
+	var average_normal: Vector3 = collision_parameters["average_normal"]
+	var average_position: Vector3 = collision_parameters["average_position"]
 
 	var particles: Node3D = collision_particles.instantiate()
 	particles.global_position = average_position
@@ -70,26 +86,43 @@ func _find_parent_collision(
 	particles.look_at(average_position + average_normal)
 
 
+func _calculate_average_collision_values(body_direct_state: PhysicsDirectBodyState3D) -> Dictionary:
+	var contact_count = body_direct_state.get_contact_count()
+	var average_position = Vector3.ZERO
+	var average_normal = Vector3.ZERO
+	for i in range(contact_count):
+		average_position += body_direct_state.get_contact_collider_position(i)
+		average_normal += body_direct_state.get_contact_local_normal(i)
+	average_position /= contact_count
+	average_normal /= contact_count
+	return {
+		"average_position": average_position,
+		"average_normal": average_normal,
+	}
+
+
 func calculate_damage(me: Node, oponent: Node) -> Damage:
-	var oponet_calculator: Node
+	var oponet_calculator
 	if oponent.has_meta("collision_damage_calculator"):
 		oponet_calculator = oponent.get_meta("collision_damage_calculator")
 	else:
 		oponet_calculator = null
 
 	# calculate base damage
-	var direction: Vector3 = me.position.direction_to(oponent.position)
+	var body_direct_state = PhysicsServer3D.body_get_direct_state(calculated_body.get_rid())
+	var collision_parameters = _calculate_average_collision_values(body_direct_state)
+
+	var direction: Vector3 = -collision_parameters["average_normal"]
 	var v1: Vector3 = self.pre_collision_velocity
 	var v2: Vector3
 	if oponet_calculator == null:
 		v2 = Vector3(0, 0, 0)
 	else:
 		v2 = oponet_calculator.pre_collision_velocity
-	var base_damage: float = (v1 - v2).dot(direction)
+	var base_damage: float = abs((v1 - v2).dot(direction)) * DAMAGE_MULTIPLIER
 
 	# get multiplies
-	var self_dmg_mult: float = 1
-	self_dmg_mult *= self.self_damage_multiplier
+	var self_dmg_mult = self.self_damage_multiplier
 	if oponet_calculator != null:
 		self_dmg_mult *= oponet_calculator.dealt_damage_multiplier
 

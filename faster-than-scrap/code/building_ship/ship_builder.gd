Cyberiada2024-2025/Faster-Than-Scrap

commit 9f71308d314f304306f139be7616676aeee66cf7

Kill all enemies mission (#508)
- Added mission to kill all enemies
- Changed labels in mission selector to RichTextLabels to support BBCode
text effects
- Fixed bug with GameManager forgetting clearing it's `ships` array when
entering/exiting a shop
- this also fixes the bug with missiles not tracking enemies after
exiting the shop
- Added tutorial

faster-than-scrap/code/scene_loader.gd
@@ -57,13 +57,14 @@ func load_fly_ship_scene(
 	var attached_fly_scene: bool = false
 
 	_detach_ship()
-	GameManager.on_scene_exit()
 	if scene_to_load != null:
+		GameManager.on_scene_exit()
 		get_tree().change_scene_to_file(scene_to_load.resource_path)
 	else:
 		if GameManager.game_state == GameState.State.BUILD:
 			attached_fly_scene = MapGenerator.swap_saved_and_current_scene()
 		if not attached_fly_scene:
+			GameManager.on_scene_exit()
 			GameManager.get_tree().change_scene_to_file("res://scenes/levels/start_level.tscn")
 			use_saved_pos_rot = false
 
@@ -96,7 +97,6 @@ func load_build_ship_scene(tutorial_version = false) -> void:
 	if GameManager.game_state == GameState.State.FLY:
 		attached_build_scene = MapGenerator.swap_saved_and_current_scene()
 
-	GameManager.on_scene_exit()
 	if tutorial_version:
 		GameManager.get_tree().change_scene_to_file(
 			"res://scenes/tutorials/build_ship_tutorial.tscn"

faster-than-scrap/code/map_select/map_manager.gd
@@ -6,7 +6,7 @@ const DEFAULT_MAP_SELECT = preload("res://prefabs/map_select/full_maps/map1.tscn
 
 @export var map_container: Control
 @export var button: Button
-@export var label: Label
+@export var label: RichTextLabel
 @export var scene_loader: SceneLoader
 
 

faster-than-scrap/code/map_select/map_selector.gd
@@ -4,7 +4,7 @@ extends HBoxContainer
 
 @export var active_node: MapNode
 
-@export var label: Label
+@export var label: RichTextLabel
 @export var scene_loader: SceneLoader
 
 var layers: Array[MapLayer] = []

faster-than-scrap/code/missions/a/mission_kill_enemies.gd
@@ -1,0 +1,28 @@
+class_name MissionKillEnemies
+
+extends Mission
+
+@export var vortex_center: Node3D
+
+var _kill_enemies_tutorial_cutscene = preload(
+	"res://prefabs/ui/cutscenes/tutorials/missions/kill_enemies_tutorial.tscn"
+)
+
+
+func setup() -> void:
+	super()
+	_spawn_vortex(vortex_center.global_position)
+
+	CutsceneManager.play_cutscene(_kill_enemies_tutorial_cutscene)
+
+
+func _process(_delta: float) -> void:
+	super(_delta)
+	if _ended():
+		return
+
+	var enemies = GameManager.find_all_enemies(TeamManager.Team.PLAYER)
+	if len(enemies) == 0:
+		print("ALL ENEMIES DEFEATED")
+		state = MissionState.FINISHED
+		finished.emit(self)

faster-than-scrap/code/missions/a/mission_kill_enemies.gd.uid
@@ -1,0 +1,1 @@
+uid://cv8qkmxin552e

faster-than-scrap/code/missions/info/mission_info.gd
@@ -6,7 +6,7 @@ extends Resource
 ## It does not process the progress of a mission.
 ## Supposed to be used in the map select.
 
-@export var about: String = ""
+@export_multiline var about: String = ""
 @export var difficulty: int
 
 

faster-than-scrap/prefabs/map_select/full_maps/map1.tscn
@@ -1,4 +1,4 @@
-[gd_scene load_steps=55 format=3 uid="uid://qemwu1rt7gtj"]
+[gd_scene load_steps=58 format=3 uid="uid://qemwu1rt7gtj"]
 
 [ext_resource type="Script" uid="uid://o52uw8n1ufdc" path="res://code/map_select/map_selector.gd" id="1_yxgtb"]
 [ext_resource type="PackedScene" uid="uid://2oa2dvray5oq" path="res://prefabs/map_select/map_layer.tscn" id="2_bp67s"]
@@ -11,7 +11,9 @@
 [ext_resource type="PackedScene" uid="uid://bvcjycf02eb4c" path="res://prefabs/npc/bosses/juggernaut/juggernaut.tscn" id="7_yxgtb"]
 [ext_resource type="PackedScene" uid="uid://dhnli7a3skvdj" path="res://prefabs/map_select/nodes/templates/missions/mission_defend.tscn" id="8_mxsq4"]
 [ext_resource type="PackedScene" uid="uid://crbesxqkbf1ss" path="res://scenes/levels/final/easy/few_drones_defend.tscn" id="9_dlw72"]
+[ext_resource type="PackedScene" uid="uid://dvdu57kxdkwq" path="res://prefabs/map_select/nodes/templates/missions/mission_kill_enemies.tscn" id="10_mxsq4"]
 [ext_resource type="PackedScene" uid="uid://ct6ykt00y3j7k" path="res://scenes/levels/final/easy/asteroids_cramped_escape.tscn" id="10_sau2r"]
+[ext_resource type="PackedScene" uid="uid://m8fjw7j6a2mk" path="res://scenes/levels/final/easy/shotgunners_kill_enemies.tscn" id="11_dlw72"]
 [ext_resource type="PackedScene" uid="uid://1ipl2ppnmckf" path="res://scenes/levels/final/easy/shotgunners_escape.tscn" id="11_f40e2"]
 [ext_resource type="PackedScene" uid="uid://cbtbiqg2rf01i" path="res://scenes/levels/final/easy/shotgunners_defend.tscn" id="11_uee5q"]
 [ext_resource type="PackedScene" uid="uid://pgmnuirpnvwb" path="res://scenes/levels/final/easy/few_turret_guns_defend.tscn" id="12_4e3x0"]
@@ -43,6 +45,13 @@ metadata/_custom_type_script = "uid://bmhwp67vae4gu"
 [sub_resource type="Resource" id="Resource_pki7u"]
 script = ExtResource("6_0a6x0")
 about = "DEFEND THE TARGET!"
+difficulty = 0
+metadata/_custom_type_script = "uid://bmhwp67vae4gu"
+
+[sub_resource type="Resource" id="Resource_dlw72"]
+script = ExtResource("6_0a6x0")
+about = "[rainbow freq=1.0 sat=0.8 val=0.8 speed=1.0]RIP AND TEAR
+UNTIL IT IS DONE[/rainbow]"
 difficulty = 0
 metadata/_custom_type_script = "uid://bmhwp67vae4gu"
 
@@ -169,7 +178,7 @@ layout_mode = 2
 
 [node name="MapNode" parent="MapLayer" node_paths=PackedStringArray("next_map_nodes") instance=ExtResource("3_s8emc")]
 layout_mode = 2
-next_map_nodes = [NodePath("../../MapLayer4/MissionEscapeFewDrones"), NodePath("../../MapLayer4/MissionDefendFewDrones"), NodePath("../../MapLayer4/MissionEscapeAsteroidsCramped")]
+next_map_nodes = [NodePath("../../MapLayer4/MissionEscapeFewDrones"), NodePath("../../MapLayer4/MissionDefendFewDrones"), NodePath("../../MapLayer4/MissionEscapeAsteroidsCramped"), NodePath("../../MapLayer4/MissionKillEnemies")]
 active = true
 
 [node name="MapMargin" parent="." instance=ExtResource("4_nrfml")]
@@ -189,6 +198,12 @@ layout_mode = 2
 mission_info = SubResource("Resource_pki7u")
 level_prefab = ExtResource("9_dlw72")
 next_map_nodes = [NodePath("../../MapLayer5/MissionDefendShotgunners"), NodePath("../../MapLayer5/Shotgunners")]
+
+[node name="MissionKillEnemies" parent="MapLayer4" node_paths=PackedStringArray("next_map_nodes") instance=ExtResource("10_mxsq4")]
+layout_mode = 2
+mission_info = SubResource("Resource_dlw72")
+level_prefab = ExtResource("11_dlw72")
+next_map_nodes = [NodePath("../../MapLayer5/Shotgunners")]
 
 [node name="MissionEscapeAsteroidsCramped" parent="MapLayer4" node_paths=PackedStringArray("next_map_nodes") instance=ExtResource("5_rwq3m")]
 layout_mode = 2

faster-than-scrap/prefabs/map_select/nodes/templates/missions/mission_kill_enemies.tscn
@@ -1,0 +1,42 @@
+[gd_scene load_steps=6 format=3 uid="uid://dvdu57kxdkwq"]
+
+[ext_resource type="Texture2D" uid="uid://cf4pm2keeo04k" path="res://art/textures/ui/mapselect/circle.png" id="1_vpkxe"]
+[ext_resource type="Script" uid="uid://c4677xbn24lv5" path="res://code/map_select/nodes/mission_node.gd" id="2_5430q"]
+[ext_resource type="Texture2D" uid="uid://drxpv7w6wu3p1" path="res://art/textures/ui/mapselect/Boss.png" id="3_4pwt0"]
+[ext_resource type="Script" uid="uid://bmhwp67vae4gu" path="res://code/missions/info/mission_info.gd" id="3_4x7dd"]
+
+[sub_resource type="Resource" id="Resource_83pbw"]
+script = ExtResource("3_4x7dd")
+about = ""
+difficulty = 0
+metadata/_custom_type_script = "uid://bmhwp67vae4gu"
+
+[node name="MissionKillEnemies" type="TextureButton"]
+modulate = Color(2.93598e-06, 0.635492, 0.537048, 1)
+anchors_preset = 15
+anchor_right = 1.0
+anchor_bottom = 1.0
+grow_horizontal = 2
+grow_vertical = 2
+size_flags_vertical = 3
+texture_normal = ExtResource("1_vpkxe")
+ignore_texture_size = true
+stretch_mode = 5
+script = ExtResource("2_5430q")
+mission_info = SubResource("Resource_83pbw")
+modulate_color = Color(0, 0.635294, 0.537255, 1)
+
+[node name="TextureRect" type="TextureRect" parent="."]
+modulate = Color(0.462291, 0.462291, 0.462291, 1)
+layout_mode = 1
+anchors_preset = 15
+anchor_right = 1.0
+anchor_bottom = 1.0
+grow_horizontal = 2
+grow_vertical = 2
+texture = ExtResource("3_4pwt0")
+expand_mode = 1
+stretch_mode = 5
+flip_v = true
+
+[connection signal="pressed" from="." to="." method="on_click"]

commit c73a140a6d8c6a4675053cefb61636e1a8746ae8

Fix shop bugs (#533)
Fixed the following shop bugs:
- tutorial shops giving money to the player every time when entered
- tutorial shops restoring their items after being exited and entered
- inventory not working (I broke it in #508 oops)
- shops throwing errors and maintaining their inventory after going to
main menu and starting a new game

Most of the bugs were caused by loading wrong shop scenes in
scene_loader.gd, or game not resetting properly and remembering some
saved values from the previous playthrough

faster-than-scrap/code/building_ship/ship_builder.gd
@@ -554,4 +554,6 @@ func _on_deny_pressed() -> void:
 
 
 func set_freeze_mode(is_frozen: bool):
+	if GameManager.player_ship == null:
+		return  # player ship can be null here if we exited to the main menu from the shop scene
 	GameManager.player_ship.freeze = is_frozen

faster-than-scrap/code/building_ship/shop.gd
@@ -134,16 +134,13 @@ func _generate_inventory() -> void:
 		)
 		obj.position = Vector3(x, 0, z)
 		obj.get_child(0).position = Vector3(0, 0, 0)
-		obj.hide_on_module_camera()
+		obj.get_child(0).hide_on_module_camera()
 		i += 1
 	_display_inventory_number()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if first_frame:
-		## linter will burn my house down if I don't use delta somewhere in this function
-		first_frame = 0 * delta
 		first_frame = false
 		bank = starting_bank + GameManager.player_ship.money
 		_on_bank_change()

faster-than-scrap/code/collision_damage_calculator.gd
@@ -134,5 +134,4 @@ func calculate_damage(me: Node, oponent: Node) -> Damage:
 	damage.value -= flat_damage_reduction
 	if damage.value <= 0:
 		damage.value = 0
-	print(damage)
 	return damage

faster-than-scrap/code/enemy/state_machine_npc.gd
@@ -45,7 +45,6 @@ func _transition_to_next_state(target_state_path: String, data: Dictionary = {})
 	# start each transition
 	for t in state.transitions:
 		t.enter(previous_state_path, data)
-	print(owner.name + "Changed state to " + state.name)
 
 
 func _process(delta: float) -> void:

faster-than-scrap/code/enemy/states/move/state_npc_movemenet/idle.gd
@@ -1,8 +1,9 @@
 class_name EnemyIdle
 extends movementState
 
+
 func enter(_previous_state_path: String, _data := {}) -> void:
-	print("enemy is idle [zzz...]")
+	pass
 
 
 func state_physics_update(_delta: float) -> void:

faster-than-scrap/code/evironment/reset.gd
@@ -1,8 +1,0 @@
-extends Button
-
-
-func _pressed() -> void:
-	GameManager.player_ship.queue_free()
-	GameManager.player_ship = preload("res://prefabs/ships/flyable_ship.tscn").instantiate()
-	MapGenerator.reset()
-	GameManager.set_game_state(GameState.State.BUILD)

faster-than-scrap/code/evironment/reset.gd.uid
@@ -1,1 +1,0 @@
-uid://bpibk0iyedtwk

faster-than-scrap/code/player/player_ship.gd
@@ -44,7 +44,6 @@ func _ready() -> void:
 	for child in get_children():
 		if child is Module:
 			modules.append(child)
-	GameManager.new_game_state.connect(on_game_change_state)
 
 	if DebugMenu.is_debug:
 		DebugMenu.toggle_player_collisions.connect(_toggle_collisions)
@@ -97,18 +96,6 @@ func change_air_resistance() -> void:
 	else:
 		linear_damp = 0
 		angular_damp = 0
-
-
-func on_game_change_state(new_state: GameState.State) -> void:
-	match new_state:
-		GameState.State.FLY:
-			pass
-		GameState.State.PAUSE:
-			pass
-		GameState.State.BUILD:
-			pass
-		GameState.State.MAIN_MENU:
-			pass
 
 
 func save_position():

faster-than-scrap/code/scene_loader.gd
@@ -10,16 +10,13 @@ var default_ship_prefab = preload("res://prefabs/ships/flyable_ship.tscn")
 
 
 func load_main_menu_scene() -> void:
-	GameManager.on_scene_exit()
+	GameManager.reset()
 
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	GameManager.set_game_state(GameState.State.MAIN_MENU)
 
 
 func _reset_game() -> void:
-	MapGenerator.reset()
-	MapSaver.reset()
-	CutsceneManager.reset_cutscenes()
 	GameManager.reset()
 
 
@@ -27,8 +24,6 @@ func _reset_game() -> void:
 
 
 func load_movement_tutorial() -> void:
-	_reset_game()
-
 	HudSpawner.spawn_hud = true
 	GameManager.set_game_state(GameState.State.FLY)
 	get_tree().change_scene_to_file("res://scenes/tutorials/basic_movement_tutorial.tscn")
@@ -55,6 +50,9 @@ func load_fly_ship_scene(
 	use_saved_pos_rot: bool = true
 ) -> void:
 	var attached_fly_scene: bool = false
+
+	if GameManager.game_state == GameState.State.BUILD:
+		InventoryManager.save_inventory()
 
 	_detach_ship()
 	if scene_to_load != null:
@@ -97,12 +95,14 @@ func load_build_ship_scene(tutorial_version = false) -> void:
 	if GameManager.game_state == GameState.State.FLY:
 		attached_build_scene = MapGenerator.swap_saved_and_current_scene()
 
-	if tutorial_version:
-		GameManager.get_tree().change_scene_to_file(
-			"res://scenes/tutorials/build_ship_tutorial.tscn"
-		)
-	elif not attached_build_scene:
-		GameManager.get_tree().change_scene_to_file("res://scenes/build_ship.tscn")
+	if not attached_build_scene:
+		if tutorial_version:
+			GameManager.get_tree().change_scene_to_file(
+				"res://scenes/tutorials/build_ship_tutorial.tscn"
+			)
+		else:
+			GameManager.get_tree().change_scene_to_file("res://scenes/build_ship.tscn")
+
 	GameManager.set_game_state(GameState.State.BUILD)
 	_attach_ship_with_hud.call_deferred()
 

faster-than-scrap/code/singletons/cutscene_manager.gd
@@ -3,6 +3,14 @@ extends Node
 var alread_played_cutscenes: Array[String] = []
 
 
+func _ready():
+	GameManager.game_reset.connect(_on_game_manager_reset)
+
+
+func _on_game_manager_reset():
+	alread_played_cutscenes.clear()
+
+
 func play_cutscene(cutscene_packed_scene: PackedScene) -> void:
 	if SettingsManager.skip_cutscenes:
 		await Engine.get_main_loop().process_frame
@@ -17,7 +25,3 @@ func play_cutscene(cutscene_packed_scene: PackedScene) -> void:
 		await cutscene.play()
 	# whatever the result delete it
 	cutscene.queue_free()
-
-
-func reset_cutscenes() -> void:
-	alread_played_cutscenes.clear()

faster-than-scrap/code/singletons/game_manager.gd
@@ -5,6 +5,7 @@ extends Node
 ## It is autoloaded and always available to be called from everywhere!
 
 signal new_game_state(new_state: GameState.State)
+signal game_reset
 
 @export var game_state: GameState.State = GameState.State.MAIN_MENU
 @export var death_screen: PackedScene
@@ -23,13 +24,16 @@ func _enter_tree() -> void:
 
 func on_scene_exit() -> void:
 	ships = []
-	if game_state == GameState.State.BUILD:
-		InventoryManager.save_inventory()
 
 
 func reset() -> void:
+	game_reset.emit()
+
 	game_over = false
 	ships = []
+
+	player_ship.queue_free()
+	player_ship = null
 
 
 func set_game_state(new_state: GameState.State) -> void:

faster-than-scrap/code/singletons/inventory_manager.gd
@@ -5,6 +5,15 @@ var max_items = 8
 var item_number = 0
 
 
+func _ready():
+	GameManager.game_reset.connect(_on_game_manager_reset)
+
+
+func _on_game_manager_reset():
+	inventory.clear()
+	item_number = 0
+
+
 func save_inventory() -> void:
 	for i in range(inventory.size()):
 		var item = inventory[i]

faster-than-scrap/code/singletons/map_generator.gd
@@ -10,6 +10,17 @@ var _scene: Node
 var _saved_scene: Node
 
 
+func _ready():
+	GameManager.game_reset.connect(_on_game_manager_reset)
+
+
+# called when game's ended
+func _on_game_manager_reset():
+	_map_node = null
+	_scene = null
+	_saved_scene = null
+
+
 func _spawn_vortex(mission_info: MissionInfo) -> void:
 	SpaceVortex.spawn_vortex(mission_info.get_mission_final_target_position())
 
@@ -45,10 +56,4 @@ func swap_saved_and_current_scene() -> bool:
 
 func set_node(new_node: MapNode) -> void:
 	_map_node = new_node
-	_scene = null
-
-
-## called when the game's ended
-func reset() -> void:
-	_map_node = null
 	_scene = null

faster-than-scrap/code/singletons/map_saver.gd
@@ -5,8 +5,13 @@ extends Node
 var map_select: MapSelector
 
 
+func _ready():
+	GameManager.game_reset.connect(_on_game_manager_reset)
+
+
+func _on_game_manager_reset():
+	map_select = null
+
+
 func save_map() -> void:
 	map_select.get_parent().remove_child(map_select)
-
-func reset() -> void:
-	map_select = null

faster-than-scrap/prefabs/pause_menu.tscn
@@ -1,9 +1,8 @@
-[gd_scene load_steps=5 format=3 uid="uid://b8vq1jqmowcwf"]
+[gd_scene load_steps=4 format=3 uid="uid://b8vq1jqmowcwf"]
 
 [ext_resource type="Script" uid="uid://cntqnnyriiru2" path="res://code/pause_menu.gd" id="1_hf53u"]
 [ext_resource type="PackedScene" uid="uid://36s8658cgl8t" path="res://scenes/settings.tscn" id="2_atv5q"]
 [ext_resource type="Script" uid="uid://16i5n2yxnlsr" path="res://code/scene_loader.gd" id="2_iyb4f"]
-[ext_resource type="Script" path="res://code/evironment/reset.gd" id="2_ntkjb"]
 
 [node name="PauseMenu" type="Control"]
 process_mode = 3
@@ -57,7 +56,6 @@ focus_neighbor_bottom = NodePath("../Resume")
 theme_override_font_sizes/font_size = 30
 text = "EXIT TO MAIN MENU
 "
-script = ExtResource("2_ntkjb")
 
 [node name="Settings" parent="." instance=ExtResource("2_atv5q")]
 visible = false
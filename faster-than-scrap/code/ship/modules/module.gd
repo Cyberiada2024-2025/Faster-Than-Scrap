e5a046e7b6d16a96a325291caab9a05e090eec62
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-07-02 21:37:45 +0200

    Added mass to modules (#548)
    Heavier ships now need more thrusters to move with the same speed and
    lighter ships now move faster.
    Each module can have a different mass (currently set to 1 in all
    modules)
    
    Adjusted recoils and asteroid masses for the fact that player ships are
    now much heavier (previously the ship always weighted 1 kg, now
    {number_of_modules} kg)

--- faster-than-scrap/code/ship/modules/module.gd
+++ faster-than-scrap/code/ship/modules/module.gd
@@ -21,6 +21,7 @@ const EXPLOSION_DISTANCE_EXPONENT = 1
 @export var is_activable: bool = true
 @export var max_hp: float = 100
 @export var hp: float = 100
+@export var mass: float = 1
 @export_category("References")
 @export var ship: Ship
 @export var attach_points: Array[Node3D] = []
@@ -181,19 +182,20 @@ func _detach_all_children(explosion_center: Vector3) -> void:
 
 ## Called when the module is attached to the ship
 func on_attach() -> void:
-	pass
+	ship.update_mass()
 
 
 ## Called just before the module is detached from the ship
 func on_detach() -> void:
-	GameManager.player_ship.modules.erase(self)
+	ship.modules.erase(self)
+	ship.update_mass()
 	ship = null  # clear refence of a ship
 	hide_on_module_camera()
 
 
 ## Called when the module is attached to a different part of the ship than it previously was
 func on_reattach() -> void:
-	pass
+	ship.update_mass()
 
 
 func deactivate() -> void:

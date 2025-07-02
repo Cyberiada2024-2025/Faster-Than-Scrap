ecc085407f4949e83a96110709212e0e26e43dc9
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-07-02 11:39:12 +0200

    Moudle tooltip choose cog image

--- faster-than-scrap/code/ui/module_tooltip.gd
+++ faster-than-scrap/code/ui/module_tooltip.gd
@@ -6,12 +6,13 @@ var module: Module
 func config(module: Module) -> void:
 	var rich_text: RichTextLabel = $MarginContainer/RichTextLabel
 
-	rich_text.text = "[b]" + module.module_name + ":[/b] "
+	rich_text.text = "[b]" + module.module_name + " :[/b] "
 	rich_text.text += (
-		"[b]" + String.num_int64(module.prize)  + ":[/b] " + "[img={width=20}]res://art/fonts/cog.png[/img]
-		[img={width=20}]res://art/fonts/cog2.png[/img]
-		[img={width=20}]res://art/fonts/cog3.png[/img]
-		[img={width=20}]res://art/fonts/cog4.png[/img]\n"
+		"[b]"
+		+ String.num_int64(module.prize)
+		+ "[/b] "
+		+ "[img={width=20}]res://art/fonts/cog3.png[/img]"
+		+ "\n"
 	)
 
-	rich_text.text += "[b]" + module.description + ":[/b] "
+	rich_text.text += "[b]" + module.description + "[/b] "

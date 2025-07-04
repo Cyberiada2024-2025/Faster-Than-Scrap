b8f2f7a052ad269597a97210d416e3e66ad81c8a
Executed419 <lucker10@interia.pl>
2025-07-04 19:37:35 +0200

    fixed bug in defend mission

--- faster-than-scrap/code/missions/a/mission_defend.gd
+++ faster-than-scrap/code/missions/a/mission_defend.gd
@@ -3,18 +3,18 @@ class_name MissionTimedDefend
 extends Mission
 
 @export_category("Main defendable")
-@export var defendable_position: Node3D
+@export var defendable_position: Array[Node3D]
 @export var time_to_defend: float = 10
 @export var defend_prefab = preload("res://prefabs/environment/mission_related/defend_target.tscn")
 
 @export_category("Small defendables")
 @export var small_defendable_position: Array[Node3D] = []
-@export var small_time_to_defend: float = 5
+@export var small_time_to_defend: float = 4
 @export var small_defend_prefab = preload(
 	"res://prefabs/environment/mission_related/defend_target_small.tscn"
 )
 
-var _capture_counter: int = 1
+var _capture_counter: int = 0
 
 var _defend_tutorial_cutscene = preload(
 	"res://prefabs/ui/cutscenes/tutorials/missions/defend_tutorial.tscn"
@@ -24,28 +24,30 @@ var _defend_tutorial_cutscene = preload(
 func setup() -> void:
 	super()
 
-	# create defendable object
-	# TODO swap to instantiating the defendable asset f.e.: ally ship
+	# create defendable objects
 
-	_capture_counter = 1
+	_capture_counter = 0
 
 	_spawn_defendable()
 	_spawn_small_defendables()
-	_spawn_vortex(defendable_position.global_position)
+	# different way to set center might be preferable
+	_spawn_vortex(defendable_position[0].global_position)
 
 	CutsceneManager.play_cutscene(_defend_tutorial_cutscene)
 
 
 func _spawn_defendable() -> void:
 	# main defendable
-	var defendable: CapturePoint = defend_prefab.instantiate()
-	MissionManager.get_tree().current_scene.add_child.call_deferred(defendable)
-	if defendable_position == null:
-		defendable_position = get_parent().get_node("PortalPosition")
+	if defendable_position.is_empty():
+		defendable_position.append(get_parent().get_node("PortalPosition"))
 
-	defendable.global_position = defendable_position.global_position
-	defendable.on_capture.connect(_on_capture)
-	defendable.capture_time = time_to_defend
+	for defendable_pos in defendable_position:
+		var defendable: CapturePoint = defend_prefab.instantiate()
+		MissionManager.get_tree().current_scene.add_child.call_deferred(defendable)
+		defendable.global_position = defendable_pos.global_position
+		defendable.on_capture.connect(_on_capture)
+		defendable.capture_time = time_to_defend
+		_capture_counter += 1
 
 
 func _spawn_small_defendables() -> void:

65be92dabb3cc1bcd7958c30ea6e0c9cdaad36da
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-07-06 01:12:59 +0200

    Unskippable slides in comic
    The whole cutscene can still be skipped

--- faster-than-scrap/code/ui/cutscenes/comic_slide.gd
+++ faster-than-scrap/code/ui/cutscenes/comic_slide.gd
@@ -4,6 +4,7 @@ extends Slide
 signal comic_panel_show_start(color_rect: ColorRect)
 signal comic_panel_show_end(color_rect: ColorRect)
 
+@export var can_skip_individual_slides: bool = true
 @export var rects: Array[ColorRect] = []
 ## how long  a comic part will be revealed
 @export_range(0, 1) var reveal_time: float
@@ -20,6 +21,9 @@ func _enter_tree() -> void:
 
 ## override to only skip single panel on enter press
 func _input(event: InputEvent) -> void:
+	if not can_skip_individual_slides:
+		return
+
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("Skip Cutscene") and playing:
 			skip_or_timer.emit()

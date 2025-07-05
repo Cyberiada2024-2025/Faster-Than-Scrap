217830682dbeeadb8056f3f1a0712a638d1931c8
Lenetis <70149832+Lenetis@users.noreply.github.com>
2025-07-05 15:48:29 +0200

    Aegis effect size

--- faster-than-scrap/code/player/aegis_effect.gd
+++ faster-than-scrap/code/player/aegis_effect.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
-@export var player: bool = true;
+@export var player: bool = true
+
+@export var aegis_radius: float = 10
 
 @export var path: String = "res://prefabs/npc/bosses/vortex/vortex_boss.tscn::ShaderMaterial_a17kq"
 
 var vortex: ShaderMaterial
-var param_name: String = "Aegis_Position"
+var position_param_name: String = "Aegis_Position"
+var radius_param_name: String = "Aegis_Radius"
+
 
 func _ready() -> void:
 	vortex = load(path)
@@ -14,5 +18,8 @@ func _ready() -> void:
 		reparent(GameManager.player_ship.cockpit)
 		position = Vector3(0, -5, 0)
 
+	vortex.set_shader_parameter(radius_param_name, aegis_radius)
+
+
 func _process(_delta: float) -> void:
-	vortex.set_shader_parameter(param_name, Vector2(global_position.x, global_position.z))
+	vortex.set_shader_parameter(position_param_name, Vector2(global_position.x, global_position.z))
